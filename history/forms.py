7b2cdc06be5f8b8d8a6d62346c022ad78e317256
Jacob Fredericksen <iacobfred@gmail.com>
2020-03-13 14:20:22 +0900

    updates

--- history/forms.py
+++ history/forms.py
@@ -2,7 +2,7 @@ from django.contrib.postgres.forms import (
     SimpleArrayField as BaseSimpleArrayField,
     # SplitArrayField
 )
-from django.forms import DateTimeField, FileField
+from django.forms import DateTimeField, FileField, ValidationError
 
 from history.widgets.historic_date_widget import HistoricDateWidget
 from history.widgets.source_file_input import SourceFileInput
@@ -21,3 +21,11 @@ class SourceFileFormField(FileField):
 
 class HistoricDateFormField(DateTimeField):
     widget = HistoricDateWidget
+
+    def clean(self, value):
+        try:
+            super().clean(value)
+        except ValidationError as e:
+            raise ValidationError(f'HistoricDateFormField validation error: {e}')
+        except Exception as e:
+            raise Exception(f'Unexpected error: {e}')
