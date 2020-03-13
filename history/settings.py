388d35058ee27dce19e7d91fe5b10c1e390f816b
Jacob Fredericksen <iacobfred@gmail.com>
2020-03-13 21:13:40 +0900

    updates

--- history/settings.py
+++ history/settings.py
@@ -401,3 +401,12 @@ X_RAPIDAPI_KEY = config('X_RAPIDAPI_KEY')
 
 # https://docs.djangoproject.com/en/3.0/ref/contrib/sites/
 SITE_ID = 1
+
+# https://docs.djangoproject.com/en/3.0/topics/email/
+# https://docs.djangoproject.com/en/3.0/ref/settings#s-email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
+EMAIL_PORT = config('EMAIL_PORT', default=25, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = True
