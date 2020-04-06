db9bbc69adf52cfba3ab131ad40d8571e07ee71c
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-06 10:07:22 +0900

    updates

--- clean_data.py
+++ clean_data.py
@@ -21,13 +21,13 @@ from sources.models import Citation
 citations = Citation.objects.using('backup').all()
 for c in citations:
     print(f'>>> {c}')
-    Citation.objects.create(
-        citation_phrase=c.citation_phrase,
-        source=c.source,
-        content_type=c.content_type,
-        object_id=c.object_id,
-        content_object=c.content_object,
-        page_number=c.page_number,
-        end_page_number=c.end_page_number,
-        position=c.position
-    )
+    # Citation.objects.create(
+    #     citation_phrase=c.citation_phrase,
+    #     source=c.source,
+    #     content_type=c.content_type,
+    #     object_id=c.object_id,
+    #     content_object=c.content_object,
+    #     page_number=c.page_number,
+    #     end_page_number=c.end_page_number,
+    #     position=c.position
+    # )
