45212930eeb71d1a7bfd2e73b3f768d38089c099
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-06 22:06:23 +0900

    updates

--- clean_data.py
+++ clean_data.py
@@ -16,7 +16,7 @@ django.setup()
 # from django.db import transaction
 # from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
-from sources.models import Citation, QSR, OSR
+from sources.models import Citation, QSR, OSR, Source
 
 occurrence = ContentType.objects.get(app_label='occurrences', model='occurrence')
 quote = ContentType.objects.get(app_label='quotes', model='quote')
@@ -26,10 +26,11 @@ osrs = OSR.objects.using('backup').all()
 
 for _qsr in qsrs:
     qsr = _qsr
+    source = Source.objects.get(pk=qsr.source.pk)
     c = Citation.objects.get_or_create(
         citation_phrase=qsr.citation_phrase,
-        source=qsr.source.pk,
-        content_type=quote.pk,
+        source=source,
+        content_type=quote,
         object_id=qsr.quote.pk,
         page_number=qsr.page_number,
         end_page_number=qsr.end_page_number,
@@ -38,10 +39,11 @@ for _qsr in qsrs:
     print(f'>>> {c}')
 
 for osr in osrs:
+    source = Source.objects.get(pk=osr.source.pk)
     c = Citation.objects.get_or_create(
         citation_phrase=osr.citation_phrase,
-        source=osr.source.pk,
-        content_type=occurrence.pk,
+        source=source,
+        content_type=occurrence,
         object_id=osr.occurrence.pk,
         page_number=osr.page_number,
         end_page_number=osr.end_page_number,
