334c1f48de93714a8e158c71f93b448ca628a328
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-06 21:55:48 +0900

    updates

--- clean_data.py
+++ clean_data.py
@@ -15,34 +15,37 @@ django.setup()
 # from history import settings
 # from django.db import transaction
 # from django.contrib.auth.models import Permission, Group
-# from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.models import ContentType
 from sources.models import Citation, QSR, OSR
+
+occurrence = ContentType.objects.get(app_label='occurrences', model='occurrence')
+quote = ContentType.objects.get(app_label='quotes', model='quote')
 
 qsrs = QSR.objects.using('backup').all()
 osrs = OSR.objects.using('backup').all()
 
 for qsr in qsrs:
     print(f'>>> {qsr}')
-    # Citation.objects.create(
-    #     citation_phrase=c.citation_phrase,
-    #     source=c.source,
-    #     content_type=c.content_type,
-    #     object_id=c.object_id,
-    #     content_object=c.content_object,
-    #     page_number=c.page_number,
-    #     end_page_number=c.end_page_number,
-    #     position=c.position
-    # )
+    Citation.objects.create(
+        citation_phrase=qsr.citation_phrase,
+        source=qsr.source,
+        content_type=quote,
+        object_id=qsr.quote.id,
+        content_object=qsr.quote,
+        page_number=qsr.page_number,
+        end_page_number=qsr.end_page_number,
+        position=qsr.position
+    )
 
 for osr in osrs:
     print(f'>>> {osr}')
-    # Citation.objects.create(
-    #     citation_phrase=c.citation_phrase,
-    #     source=c.source,
-    #     content_type=c.content_type,
-    #     object_id=c.object_id,
-    #     content_object=c.content_object,
-    #     page_number=c.page_number,
-    #     end_page_number=c.end_page_number,
-    #     position=c.position
-    # )
+    Citation.objects.create(
+        citation_phrase=osr.citation_phrase,
+        source=osr.source,
+        content_type=occurrence,
+        object_id=osr.occurrence.id,
+        content_object=osr.occurrence,
+        page_number=osr.page_number,
+        end_page_number=osr.end_page_number,
+        position=osr.position
+    )
