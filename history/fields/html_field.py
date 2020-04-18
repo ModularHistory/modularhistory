88d45f5d766c6e6cbcb549dc34392f13d6ac1096
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-18 13:48:24 +0900

    updates

--- history/fields/html_field.py
+++ history/fields/html_field.py
@@ -10,7 +10,7 @@ from tinymce.models import HTMLField as MceHTMLField
 from history.structures.html import HTML
 
 image_key_regex = r'{{\ ?image:\ ?(.+?)\ ?}}'
-citation_key_regex = r'{{\ ?citation:\ ?(.+?)\ ?}}'
+citation_key_regex = r'\ ?{{\ ?citation:\ ?(.+?)\ ?}}'
 source_key_regex = r'{{\ ?source:\ ?(.+?)\ ?}}'
 
 
