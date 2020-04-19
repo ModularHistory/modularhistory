1a9d36b49c82f760ff5e04d681e80d2185134fac
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-19 20:36:36 +0900

    updates

--- occurrences/templatetags/occurrence_detail.py
+++ occurrences/templatetags/occurrence_detail.py
@@ -1,10 +1,12 @@
 from django import template
+from django.template.context import RequestContext
+
 from occurrences.models import Occurrence
 
 register = template.Library()
 
 
-@register.inclusion_tag('occurrences/_detail.html')
-def occurrence_detail(occurrence: Occurrence):
+@register.inclusion_tag('occurrences/_detail.html', takes_context=True)
+def occurrence_detail(context: RequestContext, occurrence: Occurrence):
     # TODO: Do math to determine columns
-    return occurrence.get_context()
+    return {**context.flatten(), **occurrence.get_context()}
