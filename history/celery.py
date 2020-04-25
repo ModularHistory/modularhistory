a2e07aac82b18dc4478baea9f9d04d0734a9e18f
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-25 13:36:48 +0900

    updates

--- history/celery.py
+++ history/celery.py
@@ -7,10 +7,12 @@ from sys import stderr
 
 from celery import Celery
 # noinspection PyPackageRequirements
-from decouple import config
+# from decouple import config
 from django.core import management
-from paramiko import SSHClient
-from scp import SCPClient
+# from paramiko import SSHClient
+# from scp import SCPClient
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
 from history import settings
 
@@ -51,18 +53,29 @@ def back_up_db(self):
             print(f'Could not find a db backup file.')
             return None
         backup_file = max(files, key=os.path.getmtime)
-        # Connect to remote backup server
-        server = config('REMOTE_BACKUP_SERVER', default=None)
-        username = config('REMOTE_BACKUP_SERVER_USERNAME', default=None)
-        password = config('REMOTE_BACKUP_SERVER_PASSWORD', default=None)
-        ssh_client = SSHClient()
-        ssh_client.load_system_host_keys()
-        # ssh_client.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
-        if server:
-            print(f'Connecting to remote backup location: {server}')
-            ssh_client.connect(server, username=username, password=password)
-            # ssh_client.connect(server, username='username', password='password')
-            with SCPClient(ssh_client.get_transport()) as scp_client:
-                scp_client.put(backup_file, f'~/history/history/backups/{backup_file}')
-                # scp_client.get('test2.txt')
-            print(f'Completed remote db backup.')
+
+        # # Connect to remote backup server
+        # server = config('REMOTE_BACKUP_SERVER', default=None)
+        # username = config('REMOTE_BACKUP_SERVER_USERNAME', default=None)
+        # password = config('REMOTE_BACKUP_SERVER_PASSWORD', default=None)
+        # ssh_client = SSHClient()
+        # ssh_client.load_system_host_keys()
+        # # ssh_client.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+        # if server:
+        #     print(f'Connecting to remote backup location: {server}')
+        #     ssh_client.connect(server, username=username, password=password)
+        #     # ssh_client.connect(server, username='username', password='password')
+        #     with SCPClient(ssh_client.get_transport()) as scp_client:
+        #         scp_client.put(backup_file, f'~/history/history/backups/{backup_file}')
+        #         # scp_client.get('test2.txt')
+        #     print(f'Completed remote db backup.')
+
+        # TODO: set up Google Drive API after getting 501(c)3 status
+        google_drive_api_is_enabled = False
+        if google_drive_api_is_enabled:
+            g_auth = GoogleAuth()
+            g_auth.LocalWebserverAuth()  # Create local webserver and handle authentication
+            drive = GoogleDrive(g_auth)
+            file_to_upload = drive.CreateFile({'title': backup_file})
+            file_to_upload.SetContentFile(backup_file)
+            file_to_upload.Upload()
