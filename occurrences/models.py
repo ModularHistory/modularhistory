df85a9fe359a0114cfd5d226d49f81e4d378bdc0
Jacob Fredericksen <jfredericksen@indeed.com>
2020-04-26 17:13:47 +0900

    updates

--- occurrences/models.py
+++ occurrences/models.py
@@ -22,7 +22,7 @@ class OccurrenceImage(Model):
         'Occurrence', related_name='occurrence_images',
         on_delete=CASCADE
     )
-    image = models.ForeignKey(Image, on_delete=CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.PROTECT)
     position = models.PositiveSmallIntegerField(
         null=True, blank=True,
         help_text='Set to 0 if the image is positioned manually.'
