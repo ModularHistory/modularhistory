1311ae8f8ec9199af747ffe81d3f2551b1ade41d
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-08-24 15:46:21 +0900

    enable GitHub auth backend

--- history/settings.py
+++ history/settings.py
@@ -318,6 +318,7 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.google.GoogleOAuth',
     'social_core.backends.twitter.TwitterOAuth',
     'social_core.backends.facebook.FacebookOAuth2',
+    'social_core.backends.github.GithubOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
 # https://python-social-auth.readthedocs.io/en/latest/pipeline.html
