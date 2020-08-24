347e1911bcaa8c39a6b80978d8f22789ee673948
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-08-24 10:46:44 +0900

    update Facebook auth settings

--- history/settings.py
+++ history/settings.py
@@ -33,7 +33,7 @@ USE_PROD_DB = config('USE_PROD_DB', default=IS_PROD, cast=bool)
 TESTING = 'test' in sys.argv
 ENVIRONMENT = (Environment.PROD if IS_PROD
                else Environment.GITHUB_TEST if os.environ.get('GITHUB_WORKFLOW')
-               else Environment.DEV)
+else Environment.DEV)
 
 ADMINS = config(
     'ADMINS',
@@ -302,10 +302,15 @@ DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': config('DBBACKUP_STORAGE_LOCATION', default=None)}
 
 AUTH_USER_MODEL = 'account.User'
+LOGIN_URL = 'account/login'
+LOGOUT_URL = 'account/logout'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
-# SOCIAL DJANGO CONFIG:
 # https://python-social-auth.readthedocs.io/en/latest/configuration/django.html
-SOCIAL_AUTH_USER_MODEL = 'account.User'
+SOCIAL_AUTH_USER_MODEL = AUTH_USER_MODEL
+SOCIAL_AUTH_USER_FIELDS = ['email', 'username']
+# https://python-social-auth.readthedocs.io/en/latest/configuration/django.html#authentication-backends
 AUTHENTICATION_BACKENDS = (
     'social_core.backends.open_id.OpenIdAuth',
     'social_core.backends.google.GoogleOpenId',
@@ -315,36 +320,62 @@ AUTHENTICATION_BACKENDS = (
     'social_core.backends.facebook.FacebookOAuth2',
     'django.contrib.auth.backends.ModelBackend',
 )
-# https://python-social-auth.readthedocs.io/en/latest/configuration/django.html#personalized-configuration
+# https://python-social-auth.readthedocs.io/en/latest/pipeline.html
 SOCIAL_AUTH_PIPELINE = (
+    # Get the information we can about the user and return it in a simple
+    # format to create the user instance later. In some cases the details are
+    # already part of the auth response from the provider, but sometimes this
+    # could hit a provider API.
     'social_core.pipeline.social_auth.social_details',
+
+    # Get the social UID from whatever service we're authing thru. The UID is
+    # the unique identifier of the given user in the provider.
     'social_core.pipeline.social_auth.social_uid',
+
+    # Verify that the current auth process is valid within the current project.
+    # This is where emails and domains whitelists are applied (if defined).
     'social_core.pipeline.social_auth.auth_allowed',
+
+    # Check if the current social-account is already associated in the site.
     'social_core.pipeline.social_auth.social_user',
+
+    # Make up a username for this person. Append a random string at the end if there's any collision.
     'social_core.pipeline.user.get_username',
-    'social_core.pipeline.social_auth.associate_by_email',  # Enabled (disabled by default)
+
+    # Associate the current social details with another user account with a similar email address.
+    'social_core.pipeline.social_auth.associate_by_email',  # Note: Default settings would disable this.
+
+    # Create a user account if we haven't found one yet.
     'social_core.pipeline.user.create_user',
+
+    # Create the record that associates the social account with the user.
     'social_core.pipeline.social_auth.associate_user',
+
+    # Populate the extra_data field in the social record with the values
+    # specified by settings (and the default ones like access_token, etc).
     'social_core.pipeline.social_auth.load_extra_data',
+
+    # Update the user record with any changed info from the auth service.
     'social_core.pipeline.user.user_details',
+
     'history.social_auth.get_user_avatar',
 )
-LOGIN_URL = 'account/login'
-LOGOUT_URL = 'account/logout'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
+SOCIAL_AUTH_POSTGRES_JSONFIELD = True
+SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
 SOCIAL_AUTH_LOGIN_ERROR_URL = '/account/settings'
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
 SOCIAL_AUTH_RAISE_EXCEPTIONS = False
-SOCIAL_AUTH_POSTGRES_JSONFIELD = True
 # Fields to populate `search_fields` in admin to search for related users
 SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'last_name', 'email']
+
 SOCIAL_AUTH_TWITTER_KEY = config('SOCIAL_AUTH_TWITTER_KEY', default='')
 SOCIAL_AUTH_TWITTER_SECRET = config('SOCIAL_AUTH_TWITTER_SECRET', default='')
+
 SOCIAL_AUTH_FACEBOOK_KEY = config('SOCIAL_AUTH_FACEBOOK_KEY', default='')
 SOCIAL_AUTH_FACEBOOK_SECRET = config('SOCIAL_AUTH_FACEBOOK_SECRET', default='')
-# SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
-# SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id, name, email', }
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
+SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {'fields': 'id, name, email'}
+
 # SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('SOCIAL_AUTH_GOOGLE_OAUTH2_KEY', default='')
 # SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET', default='')
 # SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = ['email']
@@ -543,7 +574,6 @@ SETTINGS_EXPORT = [
     'MENU_ITEMS',
     'ENABLE_PATREON'
 ]
-
 
 # TODO: show debug toolbar in prod if desired
 # def show_debug_toolbar(request) -> bool:
