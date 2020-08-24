3ef425b0d99d429c03efd1eb2e561cf9e4fb663f
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-08-24 11:35:06 +0900

    updates

--- history/social_auth.py
+++ history/social_auth.py
@@ -10,19 +10,23 @@ from social_core.backends.twitter import TwitterOAuth
 def get_user_avatar(backend, response, user, *args, **kwargs):
     print('Attempting to retrieve user profile image...')
     url = None
-    if backend.name == 'facebook' or isinstance(backend, FacebookOAuth2):
-        url = f'http://graph.facebook.com/{response["id"]}/picture?type=large'
-    elif backend.name == 'twitter' or isinstance(backend, TwitterOAuth):
-        url = response.get('profile_image_url', '').replace('_normal', '')
-    elif backend.name.startswith('google') or isinstance(backend, GoogleOAuth2):
-        if response.get('image') and response['image'].get('url'):
-            url = response['image'].get('url')
-    if url:
-        if not user.avatar:  # TODO: also check if image has been updated
-            print(f'{user} has no profile image.')
-            img_temp = NamedTemporaryFile(delete=True)
-            img_temp.write(urlopen(url).read())
-            img_temp.flush()
-            user.avatar.save(f'{user.pk}', File(img_temp))
-    else:
-        print(f'Unable to determine profile picture URL for {user}')
+    try:
+        if backend.name == 'facebook' or isinstance(backend, FacebookOAuth2):
+            url = f'http://graph.facebook.com/{response["id"]}/picture?type=large'
+        elif backend.name == 'twitter' or isinstance(backend, TwitterOAuth):
+            url = response.get('profile_image_url', '').replace('_normal', '')
+        elif backend.name.startswith('google') or isinstance(backend, GoogleOAuth2):
+            if response.get('image') and response['image'].get('url'):
+                url = response['image'].get('url')
+        if url:
+            if not user.avatar:  # TODO: also check if image has been updated
+                print(f'{user} has no profile image.')
+                img_temp = NamedTemporaryFile(delete=True)
+                img_temp.write(urlopen(url).read())
+                img_temp.flush()
+                user.avatar.save(f'{user.pk}', File(img_temp))
+        else:
+            print(f'Unable to determine profile picture URL for {user}')
+    except Exception as e:
+        print(f'>>> {type(e)} in get_user_avatar: {e}')
+        raise
