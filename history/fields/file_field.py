22e413bd2c53c6098cf9738ad2397766780edf66
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-08-24 13:36:40 +0900

    updates

--- history/fields/file_field.py
+++ history/fields/file_field.py
@@ -12,27 +12,29 @@ from history.structures.source_file import TextualSourceFile
 
 
 def dedupe_files(path: str, new_file_name: Optional[str] = None):
-    full_path = join(settings.MEDIA_ROOT, path)
-    if not new_file_name:
-        raise NotImplementedError
-    else:
-        # If uploading a new file, replace an older version if one exists.
-        # TODO: Ensure this doesn't result in replacements of unrelated files that happen to have the same name.
-        new_file_name, extension = os.path.splitext(new_file_name)
-        file_names = []
-        for f in os.listdir(full_path):
-            if isfile(join(full_path, f)) and extension in f:
-                file_name = f.replace(extension, '')
-                file_names.append(file_name)
-        to_remove = []
-        for file_name in file_names:
-            if file_name == new_file_name:
-                full_file_name = f'{file_name}{extension}'
-                file_path = join(path, full_file_name)
-                to_remove.append(file_path)
-        for file_path in to_remove:
-            print(f'Removing old version of {file_path} ...')
-            os.remove(join(settings.MEDIA_ROOT, file_path))
+    # TODO: implement file dedupe for cloud storage
+    if not settings.IS_GCP:
+        full_path = join(settings.MEDIA_ROOT, path)
+        if not new_file_name:
+            raise NotImplementedError
+        else:
+            # If uploading a new file, replace an older version if one exists.
+            # TODO: Ensure this doesn't result in replacements of unrelated files that happen to have the same name.
+            new_file_name, extension = os.path.splitext(new_file_name)
+            file_names = []
+            for f in os.listdir(full_path):
+                if isfile(join(full_path, f)) and extension in f:
+                    file_name = f.replace(extension, '')
+                    file_names.append(file_name)
+            to_remove = []
+            for file_name in file_names:
+                if file_name == new_file_name:
+                    full_file_name = f'{file_name}{extension}'
+                    file_path = join(path, full_file_name)
+                    to_remove.append(file_path)
+            for file_path in to_remove:
+                print(f'Removing old version of {file_path} ...')
+                os.remove(join(settings.MEDIA_ROOT, file_path))
 
 
 def _update_filename(instance: Model, filename: str, path: str):
