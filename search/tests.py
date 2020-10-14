b2d6d857b7b0329eed6bd211aa47095a2bd09144
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-10-14 15:33:36 +0900

    updates

--- search/tests.py
+++ search/tests.py
@@ -1,22 +1,23 @@
 import pytest
-from hypothesis import given, example
-from hypothesis.strategies import text
+# from hypothesis import given, example
+# from hypothesis.strategies import text
 from django_webtest import WebTest
+from django.urls import reverse
 
 SUCCESS = 200
 
 
-@pytest.mark.django_db
-class TestNothing:
-    """TODO: add docstring."""
-
-    do_nothing: bool = True
-
-    @given(string=text())
-    @example(string='Not doing anything')
-    def test_nothing(self, string: str):
-        """TODO: add docstring."""
-        assert self.do_nothing
+# @pytest.mark.django_db
+# class TestNothing:
+#     """TODO: add docstring."""
+#
+#     do_nothing: bool = True
+#
+#     @given(string=text())
+#     @example(string='Not doing anything')
+#     def test_nothing(self, string: str):
+#         """TODO: add docstring."""
+#         assert self.do_nothing
 
 
 @pytest.mark.django_db
@@ -24,5 +25,5 @@ class TestSearch(WebTest):
     """TODO: add docstring."""
 
     def test_search(self):
-        response = self.app.get('/search/')
+        response = self.app.get(reverse('search'))
         assert response.status_code == SUCCESS
