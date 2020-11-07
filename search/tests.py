adcc8c8a1b4d485f47da10e10ebbc3859ab3bb10
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-11-07 21:36:49 +0000

    updates

--- search/tests.py
+++ search/tests.py
@@ -1,6 +1,5 @@
 import pytest
 from django.urls import reverse
-from parameterized import parameterized
 
 from modularhistory.constants.misc import ResponseCodes
 
@@ -19,12 +18,12 @@ def test_search(django_app, django_assert_max_num_queries):
 
 
 @pytest.mark.django_db
-@parameterized.expand(['conservatism', 'liberalism'])
+@pytest.mark.parametrize('query', ['conservatism', 'liberalism'])
 def test_search_with_query(django_app, django_assert_max_num_queries, query: str):
     """Test that the search page loads successfully with queries."""
     url = f'{reverse("search")}?query={query}'
     page = django_app.get(url)
     with django_assert_max_num_queries(EXPECTED_N_SQL_QUERIES):
-        django_app.get(reverse(url))
+        django_app.get(url)
     assert page.status_code == ResponseCodes.SUCCESS
     assert 'form' in page
