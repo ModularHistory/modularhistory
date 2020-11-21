ff4d59b65f4e0ccf2ab65241c925dbd05c37c5b4
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-11-21 10:48:46 -0800

    updates

--- modularhistory/storage/google_storage.py
+++ modularhistory/storage/google_storage.py
@@ -3,6 +3,7 @@ import os
 from urllib.parse import urljoin
 
 from django.conf import settings
+from django.core.exceptions import ValidationError
 from django.core.files.storage import FileSystemStorage
 from google.auth.exceptions import DefaultCredentialsError
 from storages.backends.gcloud import GoogleCloudStorage
@@ -53,6 +54,13 @@ class GoogleCloudMediaFileStorage(GoogleCloudStorage):
             raise ValueError(
                 f'Attempting to open invalid file "{name}" resulted in {err}'
             )
+
+    def save(self, name, content, max_length=None):
+        """Save a media file to Google Cloud."""
+        try:
+            super().save(name, content, max_length)
+        except DefaultCredentialsError as err:
+            raise ValidationError(f'Attempting to save file "{name}" resulted in {err}')
 
 
 # TODO
