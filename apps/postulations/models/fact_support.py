18a369be36e9af957371ccf98bb7f977c8733119
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-12-03 00:45:29 -0600

    updates

--- apps/postulations/models/fact_support.py
+++ apps/postulations/models/fact_support.py
@@ -1,6 +1,6 @@
 """Model class for fact supportations."""
 
-from django.db.models import CASCADE, ForeignKey
+from django.db import models
 
 from apps.postulations.models.fact_relation import FactRelation
 
@@ -8,13 +8,13 @@ from apps.postulations.models.fact_relation import FactRelation
 class PostulationSupport(FactRelation):
     """A supportion of a fact by another fact."""
 
-    supported_fact = ForeignKey(
+    supported_fact = models.ForeignKey(
         'postulations.Postulation',
-        on_delete=CASCADE,
+        on_delete=models.CASCADE,
         related_name='supported_fact_supports',
     )
-    supportive_fact = ForeignKey(
+    supportive_fact = models.ForeignKey(
         'postulations.Postulation',
-        on_delete=CASCADE,
+        on_delete=models.CASCADE,
         related_name='supportive_fact_supports',
     )
