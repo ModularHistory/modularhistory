26645c75dd76910abdf99017f4219cc92d9409aa
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-12-06 09:38:35 -0600

    updates

--- modularhistory/asgi.py
+++ modularhistory/asgi.py
@@ -7,14 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-import os
 from channels.auth import AuthMiddlewareStack
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from apps.chat import routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modularhistory.settings')
 
 application = ProtocolTypeRouter(
     {
