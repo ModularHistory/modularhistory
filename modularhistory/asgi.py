b67845d7ac8eec15624e8a3dbc25abccb979f45f
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-12-06 10:13:04 -0600

    updates

--- modularhistory/asgi.py
+++ modularhistory/asgi.py
@@ -7,16 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
-from channels.auth import AuthMiddlewareStack
-
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from apps.chat import routing
 
-application = ProtocolTypeRouter(
-    {
-        'http': get_asgi_application(),
-        'https': get_asgi_application(),
-        'websocket': AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
-    }
-)
+application = get_asgi_application()
