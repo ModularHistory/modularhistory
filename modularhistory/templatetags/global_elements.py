dff0a956b05cf1985e8ffeb75e21a323679502e9
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2020-12-15 16:52:22 -0600

    updates

--- modularhistory/templatetags/global_elements.py
+++ modularhistory/templatetags/global_elements.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
@@ -12,4 +13,4 @@ def global_footer():
 @register.inclusion_tag('_navbar.html')
 def global_navbar(user):
     """Load global navbar HTML into a template."""
-    return {'user': user}
+    return {'user': user, 'menu_items': settings.MENU_ITEMS}
