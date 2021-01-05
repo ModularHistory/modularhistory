c608bc1eba14be8493955231af5f2958b066d021
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-01-05 10:59:20 -0600

    rename Dockerfiles; debug

--- frontend/pages/404.jsx
+++ frontend/pages/404.jsx
@@ -1,14 +1,14 @@
 import Head from 'next/head';
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 
 
 // redirect to Django if Next does not have a page
-export default function Custom404({hostname}) {
+export default function Custom404({ hostname }) {
   const router = useRouter();
   return (
     <Head>
       <title>404</title>
-      <meta httpEquiv="refresh" content={`0; URL=${hostname}${router.asPath}`} />
+      {/* <meta httpEquiv="refresh" content={`0; URL=${hostname}${router.asPath}`} /> */}
     </Head>
   );
 }
