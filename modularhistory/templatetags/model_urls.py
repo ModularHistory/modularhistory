47fc6532c026f4f7fb33bf1df6e3cf4c57353c8c
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-02-03 13:50:55 -0800

    updates

--- modularhistory/templatetags/model_urls.py
+++ modularhistory/templatetags/model_urls.py
@@ -15,7 +15,14 @@ def get_admin_url(model_instance: Union[Dict, Model]) -> str:
     if isinstance(model_instance, dict):
         app, model = model_instance['model'].split('.')
         return reverse(f'admin:{app}_{model}_change', args=[model_instance['pk']])
-    return model_instance.get_admin_url()
+    try:
+        return model_instance.get_admin_url()
+    except AttributeError as err:
+        logging.error(
+            f'Using the `get_admin_url` filter on {model_instance} '
+            f'(of type {type(model_instance)}) resulted in {err.__class__}: {err}'
+        )
+        return ''
 
 
 @register.filter()
