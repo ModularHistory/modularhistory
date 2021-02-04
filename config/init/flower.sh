4fbbaa1fb830ba0b3c9ee81b646272f70724eb65
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-02-03 23:51:17 -0800

    updates

---
+++ config/init/flower.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+wait-for-it.sh redis:6379 -- 
+until timeout 10 celery -A modularhistory inspect ping; do
+    >&2 echo "Celery workers not available"
+done
+
+# --url_prefix=flower
+flower -A modularhistory --port=5555 --broker=redis://redis:6379/0
