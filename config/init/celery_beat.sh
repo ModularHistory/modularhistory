bb99094b1963a7bac119e2c73e2f0f75f4da5876
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-02-25 19:37:46 -0800

    write pidfile in /tmp

--- config/init/celery_beat.sh
+++ config/init/celery_beat.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 wait-for-it.sh redis:6379 -- 
-celery -A modularhistory beat -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A modularhistory beat -l INFO \
+    --scheduler django_celery_beat.schedulers:DatabaseScheduler \
+    --pidfile /tmp/celerybeat.pid
