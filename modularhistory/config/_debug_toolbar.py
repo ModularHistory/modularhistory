0e74fa53fd363be110c7e37829f35dd3f1fd31fb
Alashov Berkeli <yunus.alashow@gmail.com>
2021-03-22 00:58:44 -0500

    MH-128: Enabled debug-toolbar in dev env
    To enable: removed disqualifier for being dockerized and added static ip of dev service to internal ips
    
    Also removed mongodb requirement for django service

--- modularhistory/config/_debug_toolbar.py
+++ modularhistory/config/_debug_toolbar.py
@@ -11,20 +11,15 @@ https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html
 from django.conf import settings
 from django.http import HttpRequest
 
-from modularhistory.constants.environments import Environments
-
 
 def show_toolbar(request: HttpRequest) -> bool:
     """Determine whether to display the debug toolbar."""
     conditions = (
-        settings.DEBUG
-        and request.META.get('REMOTE_ADDR', None) in settings.INTERNAL_IPS,
+        settings.DEBUG and request.META.get('REMOTE_ADDR', None) in settings.INTERNAL_IPS,
         request.user.is_superuser,
     )
     disqualifiers = (
-        settings.TESTING,
-        # disable toolbar in Dockerized dev environment to avoid errors; TODO: fix?
-        settings.ENVIRONMENT == Environments.DEV and settings.DOCKERIZED,
+        [settings.TESTING]
     )
     if any(conditions) and not any(disqualifiers):
         return True
