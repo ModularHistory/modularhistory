3805169bf6e8ca2abe493c4ece9f3fca542ce068
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-03-28 23:46:42 -0500

    wip

--- .config/init/celery.sh
+++ .config/init/celery.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 wait-for-it.sh redis:6379 -- 
-writable_dirs=( ".backups" "media" "static" "frontend/.next" )
+writable_dirs=( ".backups" "media" "static" )
 for writable_dir in "${writable_dirs[@]}"; do
     test -w "/modularhistory/$writable_dir" || {
         echo "Celery lacks permission to write in $writable_dir; exiting."
