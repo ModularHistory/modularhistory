6246dacaf1ffc8050bf42f7b9d9f482f6fba8da4
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-03 13:05:54 -0500

    use actions-includes

--- .github/preprocess.sh
+++ .github/preprocess.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python -m actions_includes ./.github/workflows-src/cleanup.yml ./.github/tmp/cleanup.yml &&
-python -m actions_includes ./.github/workflows-src/delivery.yml ./.github/tmp/delivery.yml &&
-python -m actions_includes ./.github/workflows-src/integration.yml ./.github/tmp/integration.yml &&
-python -m actions_includes ./.github/workflows-src/seed.yml ./.github/tmp/seed.yml
+python -m actions_includes ./.github/workflows-src/cleanup.yml ./.github/workflows/cleanup.yml &&
+python -m actions_includes ./.github/workflows-src/delivery.yml ./.github/workflows/delivery.yml &&
+python -m actions_includes ./.github/workflows-src/integration.yml ./.github/workflows/integration.yml &&
+python -m actions_includes ./.github/workflows-src/seed.yml ./.github/workflows/seed.yml
