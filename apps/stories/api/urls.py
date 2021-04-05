45e5e7109b91149ab161a1366b1cffee7a7def63
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-05 15:27:46 -0500

    MH-162: add stories app

--- apps/stories/api/urls.py
+++ apps/stories/api/urls.py
@@ -4,10 +4,18 @@ from rest_framework import routers
 from apps.stories.api import views
 
 router = routers.DefaultRouter()
+<<<<<<< HEAD
 router.register(r'stories', views.StoryViewSet)
+=======
+router.register(r'stories', views.PostulationViewSet)
+>>>>>>> MH-162: add stories app
 
 app_name = 'stories'
 
 urlpatterns = [
+<<<<<<< HEAD
     path('', views.StoryListAPIView.as_view()),
+=======
+    path('', views.PostulationListAPIView.as_view()),
+>>>>>>> MH-162: add stories app
 ]
