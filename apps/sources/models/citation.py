979c41029af562ff8705e1e7ee700b0f974d4c7a
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-04 22:06:46 -0500

    remove en dash from regex pattern

--- apps/sources/models/citation.py
+++ apps/sources/models/citation.py
@@ -46,7 +46,7 @@ class PlaceholderGroups(DefaultPlaceholderGroups):
     QUOTATION = 'quotation'
 
 
-PAGE_STRING_GROUP = rf'(?P<{PlaceholderGroups.PAGE_STRING}>pp?\.\ [\d-–]+)'
+PAGE_STRING_GROUP = rf'(?P<{PlaceholderGroups.PAGE_STRING}>pp?\.\ [\d-]+)'
 QUOTATION_GROUP = rf'(?P<{PlaceholderGroups.QUOTATION}>\".+?\")'
 HTML_GROUP = rf'(?P<{PlaceholderGroups.HTML}>\S.+?)'
 citation_placeholder_pattern = rf'\ ?{OBJECT_PLACEHOLDER_REGEX}'.replace(
