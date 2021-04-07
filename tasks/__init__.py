44e677ba1f1906155bb2b6d7a1b479e23a14c86a
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-07 17:28:54 -0500

    fix missing dependency

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -7,17 +7,14 @@ from modularhistory.environment import IS_DEV
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modularhistory.settings')
 
-from . import db, media, scm, setup, tasks  # noqa: E402
+from . import db, media, qa, scm, setup, tasks  # noqa: E402
 
 # http://docs.pyinvoke.org/en/latest/api/collection.html#invoke.collection.Collection.from_module
 namespace = Collection.from_module(tasks)
 
-if IS_DEV:
-    from . import qa
-
-    namespace.add_collection(qa)
-    namespace.add_task(qa.lint)
-    namespace.add_task(qa.test)
+namespace.add_collection(qa)
+namespace.add_task(qa.lint)
+namespace.add_task(qa.test)
 
 namespace.add_collection(db)
 namespace.add_collection(media)
