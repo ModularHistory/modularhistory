1d35f46070584bc0fcde43962b90f6f684fb7ba2
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-06 20:06:23 -0500

    hot fix

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -3,23 +3,24 @@ import os
 
 from invoke import Collection
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modularhistory.settings')
-
 from modularhistory.environment import IS_DEV
 
-from . import db, media, qa, scm, setup, tasks  # noqa: E402
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modularhistory.settings')
+
+from . import db, media, scm, setup, tasks  # noqa: E402
 
 # http://docs.pyinvoke.org/en/latest/api/collection.html#invoke.collection.Collection.from_module
 namespace = Collection.from_module(tasks)
 
-namespace.add_collection(db)
-
-namespace.add_collection(media)
-
 if IS_DEV:
+    from . import qa
+
     namespace.add_collection(qa)
     namespace.add_task(qa.lint)
     namespace.add_task(qa.test)
+
+namespace.add_collection(db)
+namespace.add_collection(media)
 
 namespace.add_collection(scm)
 namespace.add_task(scm.commit)
