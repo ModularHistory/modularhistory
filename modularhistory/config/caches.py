4cb4d218e92d8815f710e28d68059b843dc542b9
Alashov Berkeli <yunus.alashow@gmail.com>
2021-04-10 18:51:04 -0500

    MH-104: Added cachalot to cache sql queries (#315)
    * MH-104: Added cachalot to cache sql queries
    
    with custom blacklisted tables
    
    * MH-104: Code format
    
    capitalized constant variable name
    
    * Changed Werkzeug dependency to dev only

--- modularhistory/config/caches.py
+++ modularhistory/config/caches.py
@@ -10,24 +10,46 @@ use_dummy_cache = config('DUMMY_CACHE', cast=bool, default=False)
 Cache = Dict[str, Any]
 CACHES: Dict[str, Cache]
 
+REDIS_CACHE = {
+    'BACKEND': 'django_redis.cache.RedisCache',
+    'LOCATION': f'{REDIS_BASE_URL}/0',
+    'OPTIONS': {
+        'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+    },
+}
+
 # https://github.com/jazzband/django-redis
 if IS_DEV and use_dummy_cache:
     CACHES = {
         'default': {
             'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
-        }
+        },
+        'cachalot': REDIS_CACHE,
     }
 else:
     CACHES = {
-        'default': {
-            'BACKEND': 'django_redis.cache.RedisCache',
-            'LOCATION': f'{REDIS_BASE_URL}/0',
-            'OPTIONS': {
-                'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            },
-        }
+        'default': REDIS_CACHE,
     }
     # https://github.com/jazzband/django-redis
     # https://docs.djangoproject.com/en/3.1/topics/http/sessions/#using-cached-sessions
     SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
     SESSION_CACHE_ALIAS = 'default'
+
+# cachalot settings
+
+CACHALOT_ENABLED = True
+CACHALOT_CACHE = 'default'  # cache name
+CACHALOT_CACHE_RANDOM = False  # caching of random order queries, i.e order_by('?')
+
+CACHALOT_UNCACHABLE_TABLES = frozenset((
+    'django_migrations',  # migrations must not be cached
+    'django_session', 'django_admin_log',
+    # celery tables
+    'django_celery_beat_clockedschedule', 'django_celery_beat_crontabschedule',
+    'django_celery_beat_crontabschedule', 'django_celery_beat_intervalschedule',
+    'django_celery_beat_intervalschedule', 'django_celery_beat_periodictask',
+    'django_celery_beat_periodictask', 'django_celery_beat_periodictasks',
+    'django_celery_beat_periodictasks', 'django_celery_beat_solarschedule',
+    'django_celery_results_chordcounter', 'django_celery_results_chordcounter',
+    'django_celery_results_taskresult', 'health_check_db_testmodel'
+))
