6cd57b30c1a50a8d1c32ed5a1a3ba0e1263d5d92
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-21 19:30:50 -0500

    wip

--- frontend/pages/donations/Error.jsx
+++ frontend/pages/donations/Error.jsx
@@ -9,11 +9,11 @@ class Error extends React.Component {
             <div className="pt-5">
                 <p className="h1 mt-5">Oops, something went wrong.</p>
                 <div className="mt-3 pt-5 h3">
-                    Sorry for your donation <b>{localStorage.getItem("donor")}</b>. There are some issues with your donation.
+                    Sorry, there are some issues with your donation.
                 </div>
                 <div className="row col-6 mt-5 mx-auto">
                     <div className="col-5 mt-5 mx-auto">
-                        <Link to="/" className="btn btn-primary  btn-lg"><b><i className="fa fa-arrow-bar-left"></i>TO THE LANDING PAGE</b></Link>
+                        <Link href="/" className="btn btn-primary  btn-lg"><b><i className="fa fa-arrow-bar-left"></i>TO THE LANDING PAGE</b></Link>
                     </div>
                 </div>
             </div>
