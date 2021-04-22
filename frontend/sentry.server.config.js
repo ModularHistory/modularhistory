0bf9fe1df3b905731973f590eb5e397d4898ce40
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-22 03:20:13 -0500

    cleanup

--- frontend/sentry.server.config.js
+++ frontend/sentry.server.config.js
@@ -6,7 +6,6 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN;
 
-
 Sentry.init({
   dsn: SENTRY_DSN || 'https://054ccb00ec274cc292f2472140f8260c@o431037.ingest.sentry.io/5380934',
   // Note: if you want to override the automatic release value, do not set a
