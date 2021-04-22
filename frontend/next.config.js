97a1c292d1801b6b140256199187dd53fa6affbd
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-22 02:51:14 -0500

    MH-199: add Sentry for Next.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
+// This file sets a custom webpack configuration to use Next.js with Sentry.
 // https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-module.exports = {
+const { withSentryConfig } = require('@sentry/nextjs');
+
+const moduleExports = {
   // Delegate static file compression to Nginx in production.
   // https://nextjs.org/docs/api-reference/next.config.js/compression
   compress: process.env.ENVIRONMENT === "prod" ? false : true,
@@ -28,3 +32,17 @@ module.exports = {
     ]
   },
 };
+
+const SentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
