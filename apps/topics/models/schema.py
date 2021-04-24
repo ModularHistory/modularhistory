cd36d31fee5e9829faa07237361340b0935986d5
aaw0151 <alexanderwilliams2@my.unt.edu>
2021-04-24 01:40:11 -0500

    MH-170: Added untracked schema.py.

---
+++ apps/topics/models/schema.py
@@ -0,0 +1,40 @@
+import graphene
+from graphene_django.types import DjangoObjectType, ObjectType
+
+from apps.topics.models.topic import Topic
+
+
+# GraphQL type for Topic model
+class TopicType(DjangoObjectType):
+    pk = graphene.Int(source='pk')  # adding private key to the fields
+
+    class Meta:
+        model = Topic
+        fields = (
+            'key',
+            'pk',
+        )
+
+
+# GraphQL query for all Topics with the fields of 'key' and 'pk'
+class Query(graphene.ObjectType):
+    all_topics = graphene.List(TopicType)
+
+    def resolve_all_topics(self, info, **kwargs):
+        return Topic.objects.all()
+
+
+topicsSchema = graphene.Schema(query=Query)
+
+
+def query_result():
+    result = topicsSchema.execute(
+        '''
+        query {
+          allTopics {
+            key
+            pk
+        }
+    }'''
+    )
+    return result.data['allTopics']
