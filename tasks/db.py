0dc8f8b93b7cf58ba7a1084fd014f7de416f5e75
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-27 00:33:34 -0500

    cleanup

--- tasks/db.py
+++ tasks/db.py
@@ -3,23 +3,12 @@
 from typing import Optional
 
 import django
-from decouple import config
-from paramiko import SSHClient
-from scp import SCPClient
 
-from core.constants.environments import Environments
 from core.utils import db
 
 from .command import command
 
 django.setup()
-
-from django.conf import settings  # noqa: E402
-
-SERVER: Optional[str] = config('SERVER', default=None)
-SERVER_SSH_PORT: Optional[int] = config('SERVER_SSH_PORT', default=22)
-SERVER_USERNAME: Optional[str] = config('SERVER_USERNAME', default=None)
-SERVER_PASSWORD: Optional[str] = config('SERVER_PASSWORD', default=None)
 
 GITHUB_API_BASE_URL = 'https://api.github.com'
 OWNER = 'modularhistory'
