c36106277beb1644c73a34ec20d384388369edc6
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-04-30 18:04:45 -0500

    fix tests

--- apps/occurrences/tests.py
+++ apps/occurrences/tests.py
@@ -1,17 +1,21 @@
 """Tests for the occurrences app."""
 
 import pytest
-from django.test import Client
+from django.urls import reverse
+from rest_framework.test import APIClient
 
-from core.tests import TestSuite
+
+@pytest.fixture
+def api_client():
+    return APIClient()
 
 
 @pytest.mark.django_db
-class OccurrencesTestSuite(TestSuite):
-    """Tests for the occurrences app."""
+class TestOccurrences:
+    """Test the occurrences app."""
 
-    def test_occurrences(self):
-        """Verify pages have 200 status."""
-        client = Client()
-        response = client.get('/api/occurrences/')
+    def test_api_view(self, api_client: APIClient):
+        """Test the occurrences API."""
+        url = reverse('occurrences_api:index')
+        response = api_client.get(url)
         assert response.status_code == 200
