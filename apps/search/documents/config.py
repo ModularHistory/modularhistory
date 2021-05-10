10f2b00925bff4e3e066e0ad2bfbe95d584b2ffd
Alashov Berkeli <yunus.alashow@gmail.com>
2021-05-09 20:23:40 -0500

    MH-166: Simplify index name generation

--- apps/search/documents/config.py
+++ apps/search/documents/config.py
@@ -1,5 +1,4 @@
 from elasticsearch_dsl import analyzer
-from core.constants.content_types import get_ct_id
 
 html_field_analyzer = analyzer(
     'html_strip',
@@ -13,4 +12,5 @@ DEFAULT_INDEX_SETTINGS = {'number_of_shards': 1, 'number_of_replicas': 0}
 
 def get_index_name_for_ct(ct_name: str):
     """Return the Elasticsearch index name for the given content type name."""
-    return get_ct_id(ct_name).app_label
+    app_label, model = ct_name.split('.')
+    return app_label
