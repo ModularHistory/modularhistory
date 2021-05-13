5d9c28a92c1b4d518a1a483f98d90707a9938132
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-05-13 09:48:54 -0500

    wip

--- apps/postulations/models/postulated_model.py
+++ apps/postulations/models/postulated_model.py
@@ -1,18 +1,11 @@
 """This module provides a `ModelWithPostulations` abstract model."""
 
-from typing import TYPE_CHECKING
-
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from polymorphic.models import PolymorphicModel
 
 from apps.sources.models.model_with_sources import ModelWithSources
-from core.models import AbstractModelMeta, Model
-
-if TYPE_CHECKING:
-    pass
 
 
-class PostulatedModel(ModelWithSources, metaclass=AbstractModelMeta):
+class PolymorphicProposition(PolymorphicModel, ModelWithSources):
     """
     Abstract base model for postulated models.
 
@@ -20,15 +13,3 @@ class PostulatedModel(ModelWithSources, metaclass=AbstractModelMeta):
     can be analyzed and determined to be true or false with some degree of certainty,
     should inherit from this model.
     """
-
-    postulation = models.OneToOneField(
-        to='postulations.Postulation',
-        on_delete=models.PROTECT,
-        verbose_name=_('postulation'),
-    )
-
-    # https://docs.djangoproject.com/en/3.1/ref/models/options/#model-meta-options
-    class Meta:
-        """Meta options for ModelWithPostulations."""
-
-        abstract = True
