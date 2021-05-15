17f8e5909639ebc5f284f0cb7065d124f64450b6
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-05-15 15:33:46 -0500

    fix migrations

--- apps/occurrences/models/__init__.py
+++ apps/occurrences/models/__init__.py
@@ -1,6 +1,6 @@
 """Public models of the occurrences app."""
 
-from .occurrence import Occurrence
+from .occurrence import Occurrence  # , NewOccurrence
 from .occurrence_chain import OccurrenceChain, OccurrenceChainInclusion
 from .occurrence_image import OccurrenceImage
 from .occurrence_relations import (
