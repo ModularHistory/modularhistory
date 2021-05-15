8538f40c78db56cfa14ca1436b8b6b6da51981e9
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-05-15 17:09:40 -0500

    wip

--- apps/occurrences/admin/occurrences.py
+++ apps/occurrences/admin/occurrences.py
@@ -1,7 +1,6 @@
 """Admin classes for occurrences."""
 
 from apps.admin import ModelAdmin, admin_site
-from apps.images.admin import AbstractImagesInline, HasMultipleImagesFilter
 from apps.occurrences import models
 from apps.occurrences.admin.occurrence_filters import (
     EntityFilter,
@@ -15,7 +14,6 @@ from apps.occurrences.admin.occurrence_inlines import (  # PostulationsInline,
     LocationsInline,
     OccurrencesInline,
 )
-from apps.propositions.admin import PropositionChildAdmin
 from apps.quotes.admin.related_quotes_inline import RelatedQuotesInline
 from apps.search.admin import SearchableModelAdmin
 from apps.sources.admin import CitationsInline
@@ -23,12 +21,11 @@ from apps.sources.admin.filters import HasMultipleCitationsFilter, HasSourceFilt
 from apps.topics.admin import RelatedTopicsInline
 from apps.topics.models.taggable_model import TopicFilter
 
+# class _ImagesInline(AbstractImagesInline):
+#     """Inline admin for an occurrence's images."""
 
-class _ImagesInline(AbstractImagesInline):
-    """Inline admin for an occurrence's images."""
-
-    model = models.NewOccurrence.images.through
-    readonly_fields = []
+#     model = models.NewOccurrence.images.through
+#     readonly_fields = []
 
 
 # class _InvolvedEntitiesInline(InvolvedEntitiesInline):
@@ -37,62 +34,62 @@ class _ImagesInline(AbstractImagesInline):
 #     model = models.NewOccurrence.involved_entities.through
 
 
-class _RelatedEntitiesInline(InvolvedEntitiesInline):
-    """Inline admin for an occurrence's images."""
+# class _RelatedEntitiesInline(InvolvedEntitiesInline):
+#     """Inline admin for an occurrence's images."""
 
-    model = models.NewOccurrence.related_entities.through
-
-
-class _LocationsInline(LocationsInline):
-    """Inline admin for an occurrence's locations."""
-
-    model = models.NewOccurrence.related_entities.through
+#     model = models.NewOccurrence.related_entities.through
 
 
-class NewOccurrenceAdmin(PropositionChildAdmin):
-    """Model admin for occurrences."""
+# class _LocationsInline(LocationsInline):
+#     """Inline admin for an occurrence's locations."""
 
-    model = models.Occurrence
+#     model = models.NewOccurrence.related_entities.through
 
-    exclude = SearchableModelAdmin.exclude + [
-        'postulations',
-        # Use inlines for m2m relations.
-        'images',
-        'related_entities',
-        'locations',
-    ]
-    inlines = [
-        RelatedQuotesInline,
-        # InvolvedEntitiesInline,
-        # LocationsInline,
-        _ImagesInline,
-        CitationsInline,
-        RelatedTopicsInline,
-    ]
-    list_display = [
-        'slug',
-        'title',
-        'summary',
-        'date_string',
-    ]
-    list_editable = ['title']
-    list_filter = [
-        'verified',
-        HasDateFilter,
-        HasQuotesFilter,
-        TopicFilter,
-        LocationFilter,
-        HasSourceFilter,
-        HasMultipleCitationsFilter,
-        HasMultipleImagesFilter,
-    ]
-    list_per_page = 10
-    ordering = ['date']
-    readonly_fields = SearchableModelAdmin.readonly_fields
-    search_fields = model.searchable_fields
 
-    # https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.date_hierarchy
-    date_hierarchy = 'date'
+# class NewOccurrenceAdmin(PropositionChildAdmin):
+#     """Model admin for occurrences."""
+
+#     model = models.Occurrence
+
+#     exclude = SearchableModelAdmin.exclude + [
+#         'postulations',
+#         # Use inlines for m2m relations.
+#         'images',
+#         'related_entities',
+#         'locations',
+#     ]
+#     inlines = [
+#         RelatedQuotesInline,
+#         # InvolvedEntitiesInline,
+#         # LocationsInline,
+#         _ImagesInline,
+#         CitationsInline,
+#         RelatedTopicsInline,
+#     ]
+#     list_display = [
+#         'slug',
+#         'title',
+#         'summary',
+#         'date_string',
+#     ]
+#     list_editable = ['title']
+#     list_filter = [
+#         'verified',
+#         HasDateFilter,
+#         HasQuotesFilter,
+#         TopicFilter,
+#         LocationFilter,
+#         HasSourceFilter,
+#         HasMultipleCitationsFilter,
+#         HasMultipleImagesFilter,
+#     ]
+#     list_per_page = 10
+#     ordering = ['date']
+#     readonly_fields = SearchableModelAdmin.readonly_fields
+#     search_fields = model.searchable_fields
+
+#     # https://docs.djangoproject.com/en/3.1/ref/contrib/admin/#django.contrib.admin.ModelAdmin.date_hierarchy
+#     date_hierarchy = 'date'
 
 
 class OccurrenceAdmin(SearchableModelAdmin):
@@ -144,5 +141,5 @@ class OccurrenceChainAdmin(ModelAdmin):
 
 
 admin_site.register(models.Occurrence, OccurrenceAdmin)
-admin_site.register(models.NewOccurrence, NewOccurrenceAdmin)
+# admin_site.register(models.NewOccurrence, NewOccurrenceAdmin)
 admin_site.register(models.OccurrenceChain, OccurrenceChainAdmin)
