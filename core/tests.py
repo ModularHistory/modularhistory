44138f63daf4799e936d6bbf04e9d1c57e22bfc3
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-05-20 23:19:11 -0500

    cleanup

--- core/tests.py
+++ core/tests.py
@@ -1,11 +1,4 @@
 from hypothesis.extra.django import TestCase as DjangoHypothesisTestSuite
-from hypothesis.extra.django import register_field_strategy
-from hypothesis.strategies import just
-
-from core.fields import HTMLField
-from core.structures import HTML
-
-register_field_strategy(HTMLField, just(HTML('lorem ipsum')))
 
 
 class TestSuite:
