0a8c842ed0d17264b8d69bc38f1753ba84ae0417
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-05-21 11:10:39 -0500

    refactoring

--- core/config/_graphene.py
+++ core/config/_graphene.py
@@ -7,12 +7,11 @@ class SentryMiddleware:
     """GraphQL middleware for error handling."""
 
     def on_error(self, error):
-        """
-        Properly capture errors during query execution and send them to Sentry.
-        Then raise the error again and let Graphene handle it.
-        """
+        """Handle errors raised during query execution."""
         if not IS_DEV:
+            # Properly capture the error and send it to Sentry.
             capture_exception(error)
+        # Raise the error again and let Graphene handle it.
         raise error
 
     def resolve(self, next, root, info, **args):
