e3ed0a646431620cb6187920f2f06106f251a588
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-05-27 14:03:56 -0500

    refactoring

--- apps/propositions/models/support.py
+++ apps/propositions/models/support.py
@@ -1,6 +1,7 @@
 """Model class for proposition supports."""
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from core.models import PositionedRelation
 
@@ -18,6 +19,10 @@ class Support(PositionedRelation):
         on_delete=models.CASCADE,
         related_name='conclusion_supports',
     )
+
+    class Meta:
+        unique_together = ['premise', 'conclusion']
+        verbose_name = _('support')
 
     def __str__(self) -> str:
         return f'{self.premise} --> {self.conclusion}'
