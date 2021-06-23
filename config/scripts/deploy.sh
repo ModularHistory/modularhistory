510c40e0d21bf2fe1ebdeb6e446aba5508e2d971
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-06-22 19:48:59 -0500

    server migration

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -8,12 +8,12 @@ echo "$CR_PAT" | docker login ghcr.io -u iacobfred --password-stdin || {
     echo "GHCR login failed."; exit 1
 }
 echo "Pulling images..."
-docker-compose pull --include-deps django react || {
+docker-compose pull --include-deps django react webserver || {
     echo "Failed to pull required images."; exit 1
 }
 echo "" && echo "Restarting server..."
 docker-compose down --remove-orphans
-containers=( "django" "celery_beat" "react" )
+containers=( "django" "celery_beat" "react" "webserver" )
 for container in "${containers[@]}"; do
     docker-compose up -d "$container"
 done
@@ -28,18 +28,18 @@ echo "Removing all images not used by existing containers... (https://docs.docke
 docker image prune -a -f
 docker system prune -f
 # Only reload Nginx if necessary.
-[ -f nginx.conf.sha ] || touch nginx.conf.sha
-last_conf_sha="$(cat nginx.conf.sha)"
-current_conf_sha="$(shasum config/nginx/prod/nginx.conf)"
-if [[ ! "$current_conf_sha" = "$last_conf_sha" ]]; then
-    echo "
-    Current nginx.conf hash ($current_conf_sha) does not match 
-    the last hash ($last_conf_sha); i.e., the config has changed.
-    "
-    echo "" && echo "Updating Nginx config..." && 
-    lxc file push nginx.conf webserver/etc/nginx/sites-available/default && 
-    echo "" && echo "Restarting Nginx server..." && 
-    lxc exec webserver -- service nginx reload
-fi
-shasum config/nginx/prod/nginx.conf > nginx.conf.sha
+# [ -f nginx.conf.sha ] || touch nginx.conf.sha
+# last_conf_sha="$(cat nginx.conf.sha)"
+# current_conf_sha="$(shasum config/nginx/prod/nginx.conf)"
+# if [[ ! "$current_conf_sha" = "$last_conf_sha" ]]; then
+#     echo "
+#     Current nginx.conf hash ($current_conf_sha) does not match 
+#     the last hash ($last_conf_sha); i.e., the config has changed.
+#     "
+#     echo "" && echo "Updating Nginx config..." && 
+#     lxc file push nginx.conf webserver/etc/nginx/sites-available/default && 
+#     echo "" && echo "Restarting Nginx server..." && 
+#     lxc exec webserver -- service nginx reload
+# fi
+# shasum config/nginx/prod/nginx.conf > nginx.conf.sha
 echo "" && echo "Done."
