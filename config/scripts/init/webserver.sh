6d03b661e998b5cf7ce3c14b6bb0b2445900b4bc
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-06-25 17:13:15 -0500

    refactoring

--- config/scripts/init/webserver.sh
+++ config/scripts/init/webserver.sh
@@ -6,7 +6,7 @@ if [ ! "$ENVIRONMENT" = dev ]; then
     mv /etc/ddclient.conf.tmp /etc/ddclient/ddclient.conf && 
     ddclient -daemon -pid /var/run/ddclient.pid;
     echo 'Renewing SSL certificates...' && 
-    certbot certonly --standalone --agree-tos -m "$CERTBOT_EMAIL" -n -d "${DOMAINS}";
+    certbot certonly --standalone --agree-tos -m "jacob@modularhistory.com" -n -d "${DOMAINS}";
     echo 'Starting cron daemon...' && 
     cron; 
 fi;
