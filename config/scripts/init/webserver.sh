f98f5d596974f6d34f390011966c001010676a43
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-06-25 16:44:58 -0500

    fix script path

--- config/scripts/init/webserver.sh
+++ config/scripts/init/webserver.sh
@@ -1,12 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ ! "$ENVIRONMENT" = dev ]; then 
-    echo 'Starting ddclient...' && envsubst < /etc/ddclient.conf > /etc/ddclient.conf.tmp && 
+    echo 'Starting ddclient...' && 
+    envsubst < /etc/ddclient.conf > /etc/ddclient.conf.tmp && 
     mv /etc/ddclient.conf.tmp /etc/ddclient/ddclient.conf && 
     ddclient -daemon -pid /var/run/ddclient.pid;
     echo 'Renewing SSL certificates...' && 
     certbot certonly --standalone --agree-tos -m "$CERTBOT_EMAIL" -n -d "${DOMAINS}";
     echo 'Starting cron daemon...' && 
     cron; 
-fi; 
+fi;
+
 nginx -g 'daemon off;'
