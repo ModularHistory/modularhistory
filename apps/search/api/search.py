7cb74c8e8b69f07f91eb6f5bb4be101a90aa6942
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-06-25 23:53:52 -0500

    refactoring

--- apps/search/api/search.py
+++ apps/search/api/search.py
@@ -12,7 +12,7 @@ from apps.search.documents.quote import QuoteDocument
 from apps.search.documents.source import SourceDocument
 
 if TYPE_CHECKING:
-    from core.models.model import ExtendedModel
+    from apps.search.models.searchable_model import SearchableModel
 
 
 SEARCHABLE_DOCUMENTS = {
@@ -29,7 +29,7 @@ class Search(DSLSearch):
     results_count: int
     results_by_id: Optional[dict]
 
-    def to_queryset(self, view) -> tuple[Sequence['ExtendedModel'], int]:
+    def to_queryset(self, view) -> tuple[Sequence['SearchableModel'], int]:
         """Resolve results from ElasticSearch to Django model instances."""
         response = self
 
