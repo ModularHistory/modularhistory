b33f3ed11daf079197745b9cb8e0dac98a512193
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-07-27 10:16:37 -0500

    content management wip

--- apps/cms/api/urls.py
+++ apps/cms/api/urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
 
-from apps.entities.api import views
 
 app_name = 'cms'
 
 urlpatterns = [
-    path('<str:directory>/<int:id>/', views.EntityListAPIView.as_view()),
+    # path('<str:directory>/<int:id>/', views.EntityListAPIView.as_view()),
 ]
