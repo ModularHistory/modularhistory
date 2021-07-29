4096a0ebc886ff5060cf499f7a81ba7e65af7f49
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-07-29 12:59:32 -0500

    content management WIP

--- apps/cms/api/views.py
+++ apps/cms/api/views.py
@@ -1,6 +1,4 @@
-from typing import Union
 
-from django.http import JsonResponse
 from github import Github
 from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView
@@ -23,6 +21,7 @@ class CreateBranch(CreateAPIView):
         target_branch: str,
         source_branch: str = 'main',
     ) -> Response:
+        """Respond to a POST request."""
         access_token = request.user.github_access_token
         client = Github(access_token)
         repo = client.get_repo('ModularHistory/modularhistory')
@@ -32,20 +31,5 @@ class CreateBranch(CreateAPIView):
         return super().post(self, request)
 
     def create(self, request: Request) -> Response:
+        """Create and save the new branch."""
         return super().create(request)
-
-
-@api_view(['GET'])
-def cms(request: Request, directory: str, id: Union[int, str]):
-    """Ensure the CSRF cookie is set correctly."""
-    access_token = request.user.github_access_token
-    if access_token:
-        try:
-            client = Github(access_token)
-            repo = client.get_repo('ModularHistory/modularhistory')
-            contents = repo.get_contents(f'{directory}/{id}.toml')
-            pull_requests = repo.get_pulls()
-            return JsonResponse({'contents': contents})
-        except AttributeError:
-            pass
-    return JsonResponse({})
