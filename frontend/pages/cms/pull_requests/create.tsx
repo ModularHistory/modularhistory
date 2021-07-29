3445a4aa0eaf9d783e21d0bdd110ea3b4e293452
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-07-28 19:39:22 -0500

    content management WIP

--- frontend/pages/cms/pull_requests/create.tsx
+++ frontend/pages/cms/pull_requests/create.tsx
@@ -1,57 +1,29 @@
-import Layout from "@/components/Layout";
-import Container from "@material-ui/core/Container";
-import DropIn from "braintree-web-drop-in-react";
-import { GetServerSideProps } from "next";
-import { useRouter } from "next/router";
-import { FC, useEffect, useState } from "react";
 import axiosWithoutAuth from "@/axiosWithoutAuth";
+import Layout from "@/components/Layout";
+import PageHeader from "@/components/PageHeader";
+import { Grid } from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import { GetServerSideProps } from "next";
+import { useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { FC } from "react";
 
 interface PullRequestProps {
   clientToken: string;
 }
 
 const PullRequest: FC<PullRequestProps> = (props: PullRequestProps) => {
-  const [clientToken, setClientToken] = useState(props.clientToken);
-  const [instance, setInstance] = useState(null);
+  const [session, _loading] = useSession();
   const router = useRouter();
   return (
-    <Layout title="Pull request">
+    <Layout title="Create pull request">
       <Container>
-        {(!clientToken && <p className="lead">Loading...</p>) || (
-          <div>
-            <header>
-              <p className="h3">Help us keep ModularHistory running</p>
-              <p>
-                ModularHistory provide its content for free. If you would like to help keep
-                ModularHistory afloat, you can use this form to make a donation.
-              </p>
-            </header>
-            <div className="row col-12 p-0 m-0 mt-3">
-              <div className="col pl-0">
-                <input
-                  type="text"
-                  className="form-control"
-                  name="name"
-                  id="name"
-                  placeholder="Name"
-                  required
-                />
-              </div>
-              <div className="col pr-0">
-                <input
-                  type="number"
-                  className="form-control"
-                  name="amount"
-                  id="amount"
-                  placeholder="Amount"
-                  required
-                />
-              </div>
-            </div>
-            <DropIn options={{ authorization: clientToken }} onInstance={(_) => setInstance(_)} />
-            <button onClick={makeDonation}>Make donation</button>
-          </div>
-        )}
+        <PageHeader>Create pull request</PageHeader>
+        <Grid container direction="row" justifyContent="space-evenly" alignItems="flex-start">
+          <Grid item sm={12} md={6} lg={6} xl={6} style={{ margin: "0 3rem" }}>
+            Are you sure?
+          </Grid>
+        </Grid>
       </Container>
     </Layout>
   );
