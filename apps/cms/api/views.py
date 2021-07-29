88fb0f43def6b6866ce600001af6342cbe079ade
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-07-29 13:00:21 -0500

    content management WIP

--- apps/cms/api/views.py
+++ apps/cms/api/views.py
@@ -1,6 +1,4 @@
-
 from github import Github
-from rest_framework.decorators import api_view
 from rest_framework.generics import CreateAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
