9e077815d28a35e929a1069845aeffd1cd987b85
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-08-21 19:42:10 -0700

    Moderated relations (#605)
    fix django init script

--- config/scripts/init/django.sh
+++ config/scripts/init/django.sh
@@ -32,12 +32,7 @@ python manage.py collectstatic --no-input || {
 }
 
 # Rebuild indexes.
-if [[ "$USE_PARALLEL_INDEX_BUILDING" = 1 ]]; then
-    parallel="-f --parallel"
-else
-    parallel="-f"
-fi
-python manage.py search_index --rebuild "$parallel" || {
+python manage.py search_index --rebuild -f || {
     echo "Failed to rebuild elasticsearch indexes."
     exit 1
 }
