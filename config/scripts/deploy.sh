89169f8570fbaab2a842fcf6cf5b31326b01eea8
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-08-27 19:30:26 -0500

    separate compose files

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -3,6 +3,9 @@
 # Note: Environment variables are set in production environment 
 # before this script is run.
 
+shopt -s expand_aliases
+alias compose='docker-compose -f docker-compose.yml -f docker-compose.prod.yml'
+
 echo "" && echo "Updating PyInvoke config..."
 cp config/invoke.yaml "$HOME/.invoke.yaml"
 echo "" && echo "Logging in to the container registry..."
@@ -10,20 +13,20 @@ echo "$CR_PAT" | docker login ghcr.io -u iacobfred --password-stdin || {
     echo "GHCR login failed."; exit 1
 }
 echo "Pulling images..."
-docker-compose pull --include-deps django react webserver || {
+compose pull --include-deps django react webserver || {
     echo "Failed to pull required images."; exit 1
 }
 echo "" && echo "Restarting server..."
-docker-compose down --remove-orphans
+compose down --remove-orphans
 containers=( "django" "celery_beat" "react" "webserver" )
 for container in "${containers[@]}"; do
-    docker-compose up -d "$container"
+    compose up -d "$container"
 done
 sleep 10
 healthy=false
 while [[ "$healthy" = false ]]; do
     healthy=true
-    [[ "$(docker-compose ps)" =~ (Exit|unhealthy|starting) ]] && healthy=false
+    [[ "$(compose ps)" =~ (Exit|unhealthy|starting) ]] && healthy=false
     if [[ "$healthy" = false ]]; then echo "Waiting for containers to be healthy..."; sleep 20; fi
 done
 echo "Removing all images not used by existing containers... (https://docs.docker.com/config/pruning/#prune-images)"
