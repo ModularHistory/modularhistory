5271e608b5c5af017ef6d63c4072e6f7bd225bc5
Matthew Fredericksen <matthewfredericksen@my.unt.edu>
2021-08-31 18:41:55 -0500

    MH-224 (#633)

--- apps/search/documents/config.py
+++ apps/search/documents/config.py
@@ -18,8 +18,3 @@ DEFAULT_INDEX_SETTINGS = {
     'number_of_shards': 1,
     'number_of_replicas': 0,
 }
-
-
-def get_index_name_for_ct(ct_name: str):
-    """Return the Elasticsearch index name for the given content type name."""
-    return ct_name.split('.')[0]
