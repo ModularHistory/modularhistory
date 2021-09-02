f4a039daf30f4caab814d2efafabb9d7058cb0e4
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-09-02 03:24:00 -0500

    flatpages

--- apps/flatpages/admin.py
+++ apps/flatpages/admin.py
@@ -1,23 +1,14 @@
-from django.utils.translation import gettext_lazy as _
 
 from apps.admin.admin_site import admin_site
-from apps.flatpages.forms import FlatPageForm
+from apps.admin.model_admin import FORM_FIELD_OVERRIDES
 from apps.flatpages.models import FlatPage
 from apps.moderation.admin.moderated_model import ModeratedModelAdmin
 
 
 class FlatPageAdmin(ModeratedModelAdmin):
-    form = FlatPageForm
-    fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
-        (
-            _('Advanced options'),
-            {
-                'classes': ('collapse',),
-                'fields': ('registration_required', 'template_name'),
-            },
-        ),
-    )
+    """Admin for flat pages."""
+
+    formfield_overrides = FORM_FIELD_OVERRIDES
     list_display = ('url', 'title')
     list_filter = ('sites', 'registration_required')
     search_fields = ('url', 'title')
