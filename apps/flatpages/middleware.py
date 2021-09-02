e619abd0ea61ede9d2cad45f3b4c1d79257a126a
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-09-01 22:08:03 -0500

    use flatpages

--- apps/flatpages/middleware.py
+++ apps/flatpages/middleware.py
@@ -1,31 +1,23 @@
 from django.conf import settings
-from django.contrib.flatpages.middleware import FlatpageFallbackMiddleware
 from django.http import Http404
+from django.utils.deprecation import MiddlewareMixin
 
 from apps.flatpages.views import flatpage
 
-HTTP404_RESPONSE_CODE = 404
 
-
-class FlatPageFallbackMiddleware(FlatpageFallbackMiddleware):
-    """
-    Override FlatpageFallbackMiddleware to use our flatpage view.
-
-    This middleware tries to serve a static page matching the request URL,
-    if no code-defined URL patterns matched the request.
-    """
+class FlatPageFallbackMiddleware(MiddlewareMixin):
+    """Middleware for falling back on serving a flat page matching the request URL."""
 
     def process_response(self, request, response):
-        """Attempt to fall back on a static page."""
-        if response.status_code != HTTP404_RESPONSE_CODE:
-            return response
+        if response.status_code != 404:
+            return response  # No need to check for a flatpage for non-404 responses.
         try:
             return flatpage(request, request.path_info)
         # Return the original response if any errors happened. Because this
         # is a middleware, we can't assume the errors will be caught elsewhere.
         except Http404:
             return response
-        except Exception as error:
+        except Exception:
             if settings.DEBUG:
-                raise error
+                raise
             return response
