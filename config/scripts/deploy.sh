d95cb3214ff926cd9d89d34a7b4169e042923eb1
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-09-02 23:24:04 -0500

    cleanup

--- config/scripts/deploy.sh
+++ config/scripts/deploy.sh
@@ -27,6 +27,8 @@ for container in "${containers[@]}"; do
 done
 
 echo "" && echo "Starting new containers..."
+# Bring new containers online, with nginx continuing to route only to the old containers. 
+docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
 docker-compose up -d --scale django=2 celery=2 celery_beat=2 next=2 --no-recreate "${containers[@]}"
 echo "" && docker-compose ps && echo "" && docker-compose logs
 healthy=false; timeout=300; interval=20; waited=0
@@ -61,35 +63,3 @@ docker image prune -a -f
 docker system prune -f
 
 echo "" && echo "Done."
-
-
-
-
-
-
-
-
-
-service_name=tines-app  
-old_container_id=$(docker ps -f name=$service_name -q | tail -n1)
-
-# bring a new container online, running new code  
-# (nginx continues routing to the old container only)  
-docker-compose up -d --no-deps --scale $service_name=2 --no-recreate $service_name
-
-# wait for new container to be available  
-new_container_id=$(docker ps -f name=$service_name -q | head -n1)
-new_container_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $new_container_id)
-curl --silent --include --retry-connrefused --retry 30 --retry-delay 1 --fail http://$new_container_ip:3000/ || exit 1
-
-# start routing requests to the new container (as well as the old)  
-reload_nginx
-
-# take the old container offline
-docker stop $old_container_id
-docker rm $old_container_id
-
-docker-compose up -d --no-deps --scale $service_name=1 --no-recreate $service_name
-
-# stop routing requests to the old container  
-reload_nginx  
