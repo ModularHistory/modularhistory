55fa63bba41c555679dddd924127805492123e72
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-09-04 17:55:55 -0500

    refactoring

--- apps/search/templatetags/highlight.py
+++ apps/search/templatetags/highlight.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.filter(is_safe=True)
 def highlight(text_body: str, text_to_highlight: str = ''):
-    """TODO: add docstring."""
+    """Within the text body, highlight instances of the text to highlight."""
     html = text_body
     for word in text_to_highlight.split(' '):
         html = html.replace(word, f'<span class="highlighted">{word}</span>')
