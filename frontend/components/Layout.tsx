5fb86c5bee89e3ebf6f4b5c0c757831417111d82
mattfredericksen <mfred98@gmail.com>
2021-09-05 20:14:48 -0500

    page transition progress bar

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,9 +1,8 @@
 import { AUTH_COOKIES } from "@/auth";
-import { Box, Fade, LinearProgress } from "@material-ui/core";
 import { useSession } from "next-auth/client";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import { FunctionComponent, PropsWithChildren, useEffect, useState } from "react";
+import { FunctionComponent, PropsWithChildren, useEffect } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
@@ -19,7 +18,6 @@ const Layout: FunctionComponent<LayoutProperties> = ({
 }: PropsWithChildren<LayoutProperties>) => {
   const router = useRouter();
   const [session, _loading] = useSession();
-  const [loadingProgress, setLoadingProgress] = useState(1);
 
   // https://next-auth.js.org/tutorials/refresh-token-rotation#client-side
   useEffect(() => {
@@ -43,55 +41,12 @@ const Layout: FunctionComponent<LayoutProperties> = ({
     }
   }, [session]);
 
-  useEffect(() => {
-    const handleRouteChangeStart = () => {
-      setLoadingProgress(20);
-      const updateProgress = () =>
-        setTimeout(() => {
-          console.log("updating progress");
-          setLoadingProgress((currentLoadingProgress) => {
-            if (![0, 75, 100].includes(currentLoadingProgress)) {
-              updateProgress();
-            }
-            return currentLoadingProgress + 5;
-          });
-        }, 500);
-      updateProgress();
-    };
-
-    const handleRouteChangeComplete = () => {
-      setLoadingProgress(100);
-      setTimeout(() => setLoadingProgress(0), 500);
-    };
-
-    router.events.on("routeChangeStart", handleRouteChangeStart);
-    router.events.on("routeChangeComplete", handleRouteChangeComplete);
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method:
-    return () => {
-      router.events.off("routeChangeStart", handleRouteChangeStart);
-      router.events.off("routeChangeComplete", handleRouteChangeComplete);
-    };
-  }, [router]);
-
   return (
     <>
       <Head>
         <title>{title || "Home"} | ModularHistory</title>
         <link rel="canonical" href={canonicalUrl || router.pathname} />
       </Head>
-      <Box
-      // sx={{
-      //   "& .MuiLinearProgress-bar": {
-      //     position: "absolute"
-      //   }
-      // }}
-      >
-        <Fade in={loadingProgress !== 0}>
-          <LinearProgress variant={"determinate"} value={loadingProgress} />
-        </Fade>
-      </Box>
       <Navbar />
       <div className="main-content">{children}</div>
       <Footer />
