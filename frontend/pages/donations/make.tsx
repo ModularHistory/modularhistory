a3ec052d1b2133ecfd9486d900f13de43126d055
mattfredericksen <mfred98@gmail.com>
2021-09-07 19:02:47 -0500

    merge with main added element refs

--- frontend/pages/donations/make.tsx
+++ frontend/pages/donations/make.tsx
@@ -1,33 +1,24 @@
 import Layout from "@/components/Layout";
 import { Button } from "@material-ui/core";
 import Container from "@material-ui/core/Container";
-import { makeStyles } from "@material-ui/styles";
 import DropIn from "braintree-web-drop-in-react";
 import { GetServerSideProps } from "next";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import { FC, MouseEventHandler, useEffect, useState } from "react";
+import { FC, MouseEventHandler, useEffect, useRef, useState } from "react";
 import axiosWithoutAuth from "../../axiosWithoutAuth";
 
-const useStyles = makeStyles({
-  root: {
-    height: "100%",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: "1.5rem",
-    marginBottom: "2rem",
-  },
-});
 interface DonateProps {
   clientToken: string;
 }
 
 const Donate: FC<DonateProps> = (props: DonateProps) => {
-  const classes = useStyles();
   const [clientToken, setClientToken] = useState(props.clientToken);
   const [instance, setInstance] = useState<any>();
   const router = useRouter();
+
+  const amountRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
+
   useEffect(() => {
     async function getClientToken() {
       await axiosWithoutAuth
@@ -44,19 +35,21 @@ const Donate: FC<DonateProps> = (props: DonateProps) => {
         },
       };
     }
+
     getClientToken();
   });
 
   const makeDonation: MouseEventHandler = (e) => {
     e.preventDefault();
+
     async function getNonce() {
       const { nonce } = await instance.requestPaymentMethod();
       try {
         // Send the nonce to the server.
         const response = await axiosWithoutAuth.post("/api/donations/process/", {
           paymentMethodNonce: nonce,
-          amount: $("#amount").val(),
-          name: $("#name").val(),
+          amount: amountRef.current?.value,
+          name: nameRef.current?.value ?? "",
         });
         if (response.data == "OK") {
           router.push("/donations/success");
@@ -69,12 +62,22 @@ const Donate: FC<DonateProps> = (props: DonateProps) => {
         console.error(err);
       }
     }
+
     getNonce();
   };
 
   return (
     <Layout title="Donate">
-      <Container>
+      <Container
+        sx={{
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginTop: "1.5rem",
+          marginBottom: "2rem",
+        }}
+      >
         {(!clientToken && <p className="lead">Loading...</p>) || (
           <div>
             <header>
@@ -87,6 +90,7 @@ const Donate: FC<DonateProps> = (props: DonateProps) => {
             <div className="row col-12 p-0 m-0 mt-3">
               <div className="col pl-0">
                 <input
+                  ref={nameRef}
                   type="text"
                   className="form-control"
                   name="name"
@@ -97,6 +101,7 @@ const Donate: FC<DonateProps> = (props: DonateProps) => {
               </div>
               <div className="col pr-0">
                 <input
+                  ref={amountRef}
                   type="number"
                   className="form-control"
                   name="amount"
@@ -105,15 +110,11 @@ const Donate: FC<DonateProps> = (props: DonateProps) => {
                   required
                 />
               </div>
-              <DropIn options={{ authorization: clientToken }} onInstance={(_) => setInstance(_)} />
-              <Button variant="contained" color="primary" onClick={makeDonation}>
-                Make donation
-              </Button>
             </div>
             <DropIn options={{ authorization: clientToken }} onInstance={setInstance} />
-            <button onClick={makeDonation} disabled={!instance}>
+            <Button variant="contained" color="primary" onClick={makeDonation}>
               Make donation
-            </button>
+            </Button>
           </div>
         )}
       </Container>
