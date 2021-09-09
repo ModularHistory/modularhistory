100f75eafe9b7c8d4800170d776c99a3e8b3d16e
Matthew Fredericksen <matthewfredericksen@my.unt.edu>
2021-09-09 10:38:58 -0500

    oops

--- apps/propositions/api/views.py
+++ apps/propositions/api/views.py
@@ -1,14 +1,30 @@
 from rest_framework import permissions
+from rest_framework.generics import RetrieveAPIView
 from core.api.views import ExtendedModelViewSet
 
-from apps.propositions.api.serializers import OccurrenceDrfSerializer
-from apps.propositions.models.occurrence import Occurrence
+from apps.moderation.serializers import get_moderated_model_serializer
+from apps.propositions.api.serializers import PropositionDrfSerializer
+from apps.propositions.models import Proposition
 
 
-class OccurrenceViewSet(ExtendedModelViewSet):
-    """API endpoint for viewing and editing occurrences."""
+class PropositionViewSet(ExtendedModelViewSet):
+    """API endpoint for viewing and editing propositions."""
 
-    queryset = Occurrence.objects.all()
-    lookup_field = 'slug'
-    serializer_class = OccurrenceDrfSerializer
+    queryset = Proposition.objects.all()
+    serializer_class = PropositionDrfSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def list(self, request):
+        # get_absolute_url for occurrences currently returns a link with "propositions"
+        # instead of "occurrences". For now, we work around this by allowing retrieval
+        # of occurrences through the propositions API, but listing should exclude occurrences.
+        self.queryset = self.queryset.filter(type='propositions.conclusions')
+        return super().list(request)
+
+
+class PropositionModerationAPIView(RetrieveAPIView):
+    """API view for retrieving a proposition for moderation."""
+
+    queryset = Proposition.objects.all()
+    serializer_class = get_moderated_model_serializer(PropositionDrfSerializer)
+    lookup_field = 'slug'
