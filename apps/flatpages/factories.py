ea97615f9d9168e891301ce90d30cbcb290c1d33
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-09-08 23:03:12 -0500

    refactoring

---
+++ apps/flatpages/factories.py
@@ -0,0 +1,27 @@
+import factory
+from django.conf import settings
+from django.contrib.sites.models import Site
+from factory.django import DjangoModelFactory
+
+from apps.flatpages import models
+
+
+class SiteFactory(DjangoModelFactory):
+    class Meta:
+        model = Site
+
+
+class FlatPageFactory(DjangoModelFactory):
+    class Meta:
+        model = models.FlatPage
+
+    @factory.post_generation
+    def sites(self, create, extracted, **kwargs):
+        if not create:
+            return
+        if extracted:
+            # A list of groups were passed in, use them
+            for site in extracted:
+                self.sites.add(site)
+        else:
+            self.sites.add(Site.objects.get(pk=settings.SITE_ID))
