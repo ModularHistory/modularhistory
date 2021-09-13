2c8aef72df9558ec6eaef676f0ac9018d56d99dd
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-09-13 10:53:04 -0500

    debugging

--- apps/redirects/apps.py
+++ apps/redirects/apps.py
@@ -11,7 +11,9 @@ class RedirectsConfig(AppConfig):
     # https://docs.djangoproject.com/en/dev/ref/applications/#django.apps.AppConfig.ready
     def ready(self) -> None:
         """Perform initialization tasks for the redirects app."""
+        from .models import Redirect
         from .tasks import write_map
 
-        delay(write_map)
+        if Redirect.objects.exists():
+            delay(write_map)
         return super().ready()
