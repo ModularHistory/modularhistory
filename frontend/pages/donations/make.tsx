b09290203e3a38e04d14dce148bc4b7769698dbe
Sanjay Prajapati <sunzae@gmail.com>
2021-09-21 17:19:33 -0500

    MH-204: Added PayPal and Venmo Payment methods

--- frontend/pages/donations/make.tsx
+++ frontend/pages/donations/make.tsx
@@ -109,7 +109,7 @@ const MakeDonationPage: FC<DonateProps> = (props: DonateProps) => {
                   />
                 </div>
               </div>
-              <DropIn options={{ authorization: clientToken, paypal: {flow: 'vault'}, venmo: {allowNewBrowserTab: false} }} onInstance={setInstance} />
+              <DropIn options={{ authorization: clientToken, paypal: {flow: 'vault'}, venmo: {allowNewBrowserTab: false, allowDesktop: true} }} onInstance={setInstance} />
               <Button variant="contained" color="primary" onClick={makeDonation}>
                 Make donation
               </Button>
