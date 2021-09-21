5ae2ae8678edb04d6f8c9fb618d478efc6c7eebc
Sanjay Prajapati <sunzae@gmail.com>
2021-09-21 14:31:46 -0500

    MH-204: Added the Paypal method

--- frontend/pages/donations/make.tsx
+++ frontend/pages/donations/make.tsx
@@ -109,7 +109,7 @@ const MakeDonationPage: FC<DonateProps> = (props: DonateProps) => {
                   />
                 </div>
               </div>
-              <DropIn options={{ authorization: clientToken }} onInstance={setInstance} />
+              <DropIn options={{ authorization: clientToken, paypal: {flow: 'vault'}, venmo: {allowNewBrowserTab: false} }} onInstance={setInstance} />
               <Button variant="contained" color="primary" onClick={makeDonation}>
                 Make donation
               </Button>
