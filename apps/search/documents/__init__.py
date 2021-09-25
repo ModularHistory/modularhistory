93fce3badb29236db32756ce5353bf42f5c185f1
Matthew Fredericksen <matthewfredericksen@my.unt.edu>
2021-09-25 00:00:29 -0500

    topics instant search (#747)

--- apps/search/documents/__init__.py
+++ apps/search/documents/__init__.py
@@ -1,5 +1,6 @@
-from .entity import EntityDocument
+from .entity import EntityDocument, EntityInstantSearchDocument
 from .image import ImageDocument
 from .occurrence import OccurrenceDocument
 from .quote import QuoteDocument
 from .source import SourceDocument
+from .topic import TopicInstantSearchDocument
