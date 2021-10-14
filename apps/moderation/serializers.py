412ee6278093bb48153c560b23f867618c82c938
Alashov Berkeli <yunus.alashow@gmail.com>
2021-10-13 22:16:00 -0500

    MH-230: temporary fixes for some bugs
    Moderation not saving parent fields when updating because of possible bug in django serializer. Added a temporary fix that uses 'fields' instead of 'local_fields' to get inherited fields
    
    SoftDeletable.delete was looping when trying to delete related objects of child models like Article -> Source. Caused by utls.related_objects returning the model itself.

--- apps/moderation/serializers.py
+++ apps/moderation/serializers.py
@@ -1,3 +1,4 @@
+from django.core.serializers.python import Serializer
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
@@ -16,3 +17,69 @@ def get_moderated_model_serializer(
             fields = model_serializer.Meta.fields + ['fields']
 
     return ModeratedModelSerializer
+
+
+class PythonSerializer(Serializer):
+    def serialize(
+        self,
+        queryset,
+        *,
+        stream=None,
+        fields=None,
+        use_natural_foreign_keys=False,
+        use_natural_primary_keys=False,
+        progress_output=None,
+        object_count=0,
+        **options
+    ):
+        """
+        Serialize a queryset.
+        """
+        self.options = options
+
+        self.stream = stream if stream is not None else self.stream_class()
+        self.selected_fields = fields
+        self.use_natural_foreign_keys = use_natural_foreign_keys
+        self.use_natural_primary_keys = use_natural_primary_keys
+        progress_bar = self.progress_class(progress_output, object_count)
+
+        self.start_serialization()
+        self.first = True
+        for count, obj in enumerate(queryset, start=1):
+            self.start_object(obj)
+            # Use the concrete parent class' _meta instead of the object's _meta
+            # This is to avoid local_fields problems for proxy models. Refs #17717.
+            concrete_model = obj._meta.concrete_model
+            # When using natural primary keys, retrieve the pk field of the
+            # parent for multi-table inheritance child models. That field must
+            # be serialized, otherwise deserialization isn't possible.
+            if self.use_natural_primary_keys:
+                pk = concrete_model._meta.pk
+                pk_parent = pk if pk.remote_field and pk.remote_field.parent_link else None
+            else:
+                pk_parent = None
+
+            # patch: local_fields -> fields to get all fields including inherited
+            for field in concrete_model._meta.fields:
+                if field.serialize or field is pk_parent:
+                    if field.remote_field is None:
+                        if (
+                            self.selected_fields is None
+                            or field.attname in self.selected_fields
+                        ):
+                            self.handle_field(obj, field)
+                    else:
+                        if (
+                            self.selected_fields is None
+                            or field.attname[:-3] in self.selected_fields
+                        ):
+                            self.handle_fk_field(obj, field)
+            for field in concrete_model._meta.local_many_to_many:
+                if field.serialize:
+                    if self.selected_fields is None or field.attname in self.selected_fields:
+                        self.handle_m2m_field(obj, field)
+            self.end_object(obj)
+            progress_bar.update(count)
+            self.first = self.first and False
+        self.end_serialization()
+        return self.getvalue()
