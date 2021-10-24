24e5829955869ef1f4e5d63b8d81ce9fa0eebdd6
Sanjay Prajapati <sunzae@gmail.com>
2021-10-24 13:54:37 -0500

    Made changes to the UI component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,10 @@
 import Layout from "@/components/Layout";
-import PageHeader from "@/components/PageHeader";
 import SearchButton from "@/components/search/SearchButton";
-import { Box, Container, Divider, Grid, Link, Paper } from "@mui/material";
+import { Box, Button, Container, Divider, Grid, Link } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 import { useRouter } from "next/router";
 import { FC, MouseEventHandler, useState } from "react";
 
@@ -56,43 +56,84 @@ export default function Home() {
 
   return (
     <Layout title={"Home"}>
-      <Box
-        sx={{
-          flex: "1 1",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          margin: "1.5rem 1rem 1.5rem 1rem",
-        }}
-      >
-        <Card elevation={5}>
-          <CardContent>
-            <Container>
-              <p>Search modules by topic, entity, or keywords.</p>
-              {searchForm}
-            </Container>
-          </CardContent>
-        </Card>
-      </Box>
-      <AboutModularHistory />
+      <Grid container spacing={2} columns={16} alignItems={"center"} justifyContent={"center"}>
+        <Grid item xs={8} alignItems="center" justifyContent="center">
+          <Container>
+            <AboutModularHistory />
+          </Container>
+        </Grid>
+        <Grid item xs={8}>
+          <Container>
+            <Box
+              sx={{
+                flex: "1 1",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                margin: "1.5rem 1rem 1.5rem 1rem",
+              }}
+            >
+              <Card elevation={5}>
+                <CardContent>
+                  <Container>
+                    <p>Search modules by topic, entity, or keywords.</p>
+                    {searchForm}
+                  </Container>
+                </CardContent>
+              </Card>
+            </Box>
+          </Container>
+        </Grid>
+      </Grid>
     </Layout>
   );
 }
 
-const AboutModularHistory: FC  = () => {
-  return(
-    <Container sx={{marginBottom: "20px"}}>
-      <PageHeader>About Modularhistory</PageHeader>
-      <Paper sx={{p: 2, margin: "0 auto", maxWidth: "80vw", flexGrow: 1}}>
-        <Grid container spacing = {2}>
-          <Grid item xs>
-            <p>ModularHistory is a 501(c)(3) nonprofit organization dedicated to helping people learn about and understand the history of our world, our society, and issues of modern sociopolitical discourse.</p>
-            <Divider variant = "middle" />
-            <p>ModularHistory provide its content for free.</p>
-            <p>To support us, <Link href = "/donations" variant = "body2">{' donate now.'}</Link></p>
+const AboutModularHistory: FC = () => {
+  return (
+    <Box
+      sx={{
+        flex: "1 1",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        margin: "1.5rem 1rem 1.5rem 1rem",
+        p: 4,
+      }}
+    >
+      <Card elevation={5}>
+        <Box sx={{ m: 3 }}>
+          <Grid container alignItems="center" justifyContent="center">
+            <Grid item>
+              <Typography variant="h6" gutterBottom component="div" fontWeight="bold">
+                About ModularHistory
+              </Typography>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
-    </Container>
+          <p>
+            ModularHistory is a nonprofit organization dedicated to helping people learn about and
+            understand the history of our world, our society, and issues of modern sociopolitical
+            discourse.
+          </p>
+          <Button variant="contained" href="/about/">
+            Learn More
+          </Button>
+        </Box>
+        <Divider variant="middle" />
+        <Box sx={{ m: 3 }}>
+          <Grid container alignItems="center">
+            <Grid item>
+              <p>ModularHistory provide its content for free.</p>
+              <p>
+                To support us,{" "}
+                <Link href="/donations" variant="body2">
+                  {" donate now."}
+                </Link>
+              </p>
+            </Grid>
+          </Grid>
+        </Box>
+      </Card>
+    </Box>
   );
 };
