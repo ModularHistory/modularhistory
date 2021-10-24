95ff5ed8564c86d4572de8690de476c609abfb7a
aaw0151 <alexanderwilliams2@my.unt.edu>
2021-10-24 15:20:18 -0500

    MH-253: Added subscription notification to the homepage.

--- frontend/pages/index.page.tsx
+++ frontend/pages/index.page.tsx
@@ -1,13 +1,19 @@
 import Layout from "@/components/Layout";
 import SearchButton from "@/components/search/SearchButton";
-import { Box } from "@mui/material";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import Container from "@mui/material/Container";
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useRouter } from "next/router";
-import { MouseEventHandler, useRef } from "react";
+import React, { MouseEventHandler, useRef } from "react";
 
 export default function Home() {
   const router = useRouter();
@@ -20,6 +26,10 @@ export default function Home() {
       pathname: "/search/",
       query: { query: queryInputRef.current?.value },
     });
+  };
+
+  const submitSubscription = () => {
+    return;
   };
 
   const searchForm = (
@@ -48,6 +58,52 @@ export default function Home() {
     </Grid>
   );
 
+  const subscriptionForm = (
+    <Grid
+      container
+      direction={"column"}
+      spacing={2}
+      alignItems={"center"}
+      justifyContent={"center"}
+      marginTop={"0rem"}
+      marginBottom={"1rem"}
+    >
+      <Grid item>
+        <TextField
+          inputRef={queryInputRef}
+          id={"id_subscribeEmail"}
+          name={"subscribeEmail"}
+          variant={"outlined"}
+          size={"small"}
+          style={{ minWidth: "280px", marginRight: "1rem" }}
+          onKeyPress={submitSubscription}
+          label={"Email address"}
+        />
+        <Button variant="contained" sx={{ minHeight: "40px", height: "100%" }}>
+          Sign up!
+        </Button>
+      </Grid>
+    </Grid>
+  );
+
+  const subscribeDisclaimer = (
+    <Grid container>
+      <Box sx={{ fontWeight: "100" }}>
+        <Typography style={{ fontSize: "10pt" }}>
+          By entering your details, you are agreeing to ModularHistory{" "}
+          <Link href="/privacy">
+            <a>privacy policy</a>
+          </Link>{" "}
+          and{" "}
+          <Link href="/terms">
+            <a>terms of use</a>
+          </Link>
+          . You can unsubscribe at any time.
+        </Typography>
+      </Box>
+    </Grid>
+  );
+
   return (
     <Layout title={"Home"}>
       <Box
@@ -65,6 +121,38 @@ export default function Home() {
               <p>Search modules by topic, entity, or keywords.</p>
               {searchForm}
             </Container>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "1.5rem 1rem 1.5rem 1rem",
+        }}
+        hidden
+      >
+        <Card elevation={5}>
+          <CardContent>
+            <Grid
+              item
+              sx={{
+                alignItems: "center",
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <Typography style={{ fontSize: "20pt", fontWeight: "bold" }}>
+                Sign up for the weekly ModularHistory newsletter
+              </Typography>
+              <Button>
+                <CloseIcon />
+              </Button>
+            </Grid>
+            <Typography>Sign up to recieve our newsletter!</Typography>
+            {subscriptionForm}
+            {subscribeDisclaimer}
           </CardContent>
         </Card>
       </Box>
