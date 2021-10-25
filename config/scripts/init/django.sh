fa2a74858f0da4a590d77b7546ee728d0be5b0e7
mattfredericksen <mfred98@gmail.com>
2021-10-25 14:52:52 -0500

    install dependencies on django init

--- config/scripts/init/django.sh
+++ config/scripts/init/django.sh
@@ -4,6 +4,8 @@ wait-for-it.sh postgres:5432 -- wait-for-it.sh elasticsearch:9200 --
 
 # python manage.py cleanup_django_defender  # TODO
 
+poetry install
+
 # Create a db backup, if there are any migrations to apply.
 python manage.py migrate --check || {
     [[ "$ENVIRONMENT" = prod ]] && {
