6f123352fbff2a6b11d9954375a526eea868d03f
Sanjay Prajapati <sunzae@gmail.com>
2021-10-26 12:16:16 -0500

    refactoring; added missing urlpattern

--- apps/home/api/urls.py
+++ apps/home/api/urls.py
@@ -5,5 +5,5 @@ from apps.home.api import views
 app_name = 'home'
 
 urlpatterns = [
-    path('', views.TodayInHistoryViewSet.as_view()),
+    path('todayinhistory/', views.TodayInHistoryViewSet.as_view()),
 ]
