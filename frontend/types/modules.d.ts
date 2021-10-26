64cc839122b67370d13559f2accb05a680d28506
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-10-26 13:45:30 -0500

    MH-97: add meta tags

--- frontend/types/modules.d.ts
+++ frontend/types/modules.d.ts
@@ -118,6 +118,7 @@ export type ModuleUnion = Image | Quote | Occurrence | Source | Entity | Proposi
 
 export interface FlatPage {
   title: string;
+  metaDescription: string;
   content: string;
   path: string;
 }
