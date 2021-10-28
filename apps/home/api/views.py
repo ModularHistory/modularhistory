52da3c1531eee7ebc281a9acdb477738f7dc5416
sunzae99 <70658409+sunzae99@users.noreply.github.com>
2021-10-28 17:20:16 -0500

    MH-251: Added "Today in History" component (#813)
    * MH-251: Added carousel for "Today in History"
    
    * refactoring; added missing urlpattern
    
    * fix cypress integration tests
    
    * add CYPRESS_baseUrl setting
    
    * add __init__.py files in the home app
    
    * add home api path
    
    * carousel --> grid; remove react-multi-carousel
    
    * update jest tests with TIH request handling
    
    * update TIH to pass jest
    
    * add tests for TIH
    
    * update TIH to pass tests
    
    Co-authored-by: mattfredericksen <mfred98@gmail.com>
    Co-authored-by: Jacob Fredericksen <jacob.t.fredericksen@gmail.com>

---
+++ apps/home/api/views.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from itertools import chain
+
+from django.db.models import Q
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from apps.entities.models import Entity
+from apps.propositions.models import Occurrence
+from apps.quotes.models import Quote
+
+
+class TodayInHistoryView(APIView):
+    """API endpoint for today in history."""
+
+    def get(self, request):
+        today = datetime.now()
+        # today = datetime(2021, 7, 7)  # for testing
+        entities = Entity.objects.filter(
+            Q(
+                birth_date__month=today.month,
+                birth_date__day=today.day,
+            )
+            | Q(
+                death_date__month=today.month,
+                death_date__day=today.day,
+            )
+        )
+        occurrences = Occurrence.objects.filter(date__month=today.month, date__day=today.day)
+        quotes = Quote.objects.filter(date__month=today.month, date__day=today.day)
+        serialized_results = [
+            instance.serialize() for instance in chain(entities, occurrences, quotes)
+        ]
+        return Response(serialized_results)
