fe65ab4a394e3843747caac907346032248c9e55
Alashov Berkeli <yunus.alashow@gmail.com>
2021-10-28 05:34:31 -0500

    MH-270: improve quote api tests

--- apps/quotes/api/tests.py
+++ apps/quotes/api/tests.py
@@ -12,21 +12,38 @@ from apps.users.models import User
 
 
 @pytest.fixture(scope='class')
-def quote_class(request):
-
+def quote_api_test_data(request):
     request.cls.contributor = UserFactory.create()
     request.cls.verified_quote = QuoteFactory.create(verified=True)
 
+    request.cls.uncheckable_fields = ['date']
+    request.cls.test_data = {
+        'title': 'Test Quote TITLE',
+        'text': 'Test Quote TEXT',
+        'date': '2001-01-01 01:01:20.086200',
+    }
+    request.cls.updated_test_data = {
+        'title': 'UPDATED QUOTE TITLE',
+        'slug': 'updated-slug',
+        'text': 'UPDATED Test Quote TEXT',
+        'bite': 'UPDATED Test Quote BITE',
+        'date': '2001-01-01 01:01:20.086200',
+    }
 
-@pytest.mark.usefixtures('quote_class')
-class TestQuotesApi(APITestCase):
+
+@pytest.mark.usefixtures('quote_api_test_data')
+class QuotesApiTest(APITestCase):
     """Test the quotes api."""
 
+    api_client = APIClient()
     api_name = 'quotes_api'
 
-    api_client = APIClient()
-
     contributor: User
+
+    uncheckable_fields: list
+    test_data: dict
+    updated_test_data: dict
+
     verified_quote: Quote
 
     def api_view_get_test(self, view, url_kwargs=None, status_code=200):
@@ -34,7 +51,19 @@ class TestQuotesApi(APITestCase):
         response = self.api_client.get(path)
         assert response.status_code == status_code
 
-    def api_create_view_test(self, data, view='api-root', url_kwargs=None):
+    def api_moderation_view_test(
+        self,
+        data,
+        view='api-root',
+        url_kwargs=None,
+        change_status_code=200,
+        method='post',
+        object_id=None,
+    ):
+        if url_kwargs is None:
+            url_kwargs = {}
+        if object_id:
+            url_kwargs.update({'pk_or_slug': object_id})
         path = reverse(f'{self.api_name}:{view}', kwargs=url_kwargs)
 
         self.assertEqual(
@@ -44,49 +73,94 @@ class TestQuotesApi(APITestCase):
         )
 
         self.api_client.force_authenticate(self.contributor)
-        response = self.api_client.post(path, data)
+
+        api_request = getattr(self.api_client, method)
+        response = api_request(path, data)
+
         self.api_client.logout()
 
-        self.assertEqual(response.status_code, 201, 'Object creation successful')
+        self.assertEqual(
+            response.status_code, change_status_code, 'Change status code is not correct'
+        )
+
+        if response.data and 'id' in response.data:
+            object_id = response.data.get('id')
 
         created_change = Change.objects.get(
-            initiator=self.contributor, object_id=response.data.get('id')
+            initiator=self.contributor,
+            object_id=object_id,
         )
 
         contributions = ContentContribution.objects.filter(
             contributor=self.contributor, change_id=created_change
         )
 
-        # currently fails, but it shouldn't..
-        assert contributions.count() == 0
+        # TODO: find out why multiple contributions are created
+        self.assertGreater(
+            contributions.count(),
+            0,
+            'Incorrect count of Contributions were created for a change',
+        )
 
-        # currently doesn't fail, it should..
-        assert contributions[0].content_before == contributions[1].content_before
+        return response.data, created_change, contributions
 
-        return response.data
+    def api_moderation_change_test(self, request_params):
+        (response, change, contribution) = self.api_moderation_view_test(**request_params)
+        for key, value in request_params.get('data').items():
+            if key not in self.uncheckable_fields:
+                self.assertEqual(
+                    getattr(change.changed_object, key),
+                    value,
+                    f'{key} field was not updated correctly',
+                )
 
     def test_api_list(self):
-        """Test the quotes API listing."""
+        """Test the quotes listing API."""
         self.api_view_get_test('quote-list')
 
     def test_api_detail(self):
+        """Test the quotes detail API."""
         self.api_view_get_test(
             'quote-detail', url_kwargs={'pk_or_slug': self.verified_quote.id}
         )
 
     def test_api_create(self):
-        quote_data = {
-            'title': 'Test Quote TITLE',
-            'text': 'Test Quote TEXT',
-            'date': '2001-01-01 01:01:20.086200',
+        """Test the quotes creation API."""
+        request_params = {'data': self.test_data, 'change_status_code': 201}
+        self.api_moderation_change_test(request_params)
+
+    def test_api_update(self):
+        """Test the quotes update API."""
+        request_params = {
+            'data': self.updated_test_data,
+            'object_id': self.verified_quote.id,
+            'view': 'quote-detail',
+            'method': 'put',
         }
-        entity_data = {
-            'type': 'entities.person',
-            'name': 'Test Entity John Doe 1100',
-            'unabbreviatedName': 'John Doe 1100',
-            'description': '<p>Test Entity description </p>',
-            'aliases': ['Jane Doe', 'John The Baptist'],
-            'birthDate': '0001-01-01 01:01:20.086200',
-            'deathDate': '2066-06-06 05:03:02',
+
+        self.api_moderation_change_test(request_params)
+
+    def test_api_patch(self):
+        """Test the quotes patch API."""
+        request_params = {
+            'data': self.updated_test_data,
+            'object_id': self.verified_quote.id,
+            'view': 'quote-detail',
+            'method': 'patch',
         }
-        self.api_create_view_test(data=quote_data)
+
+        self.api_moderation_change_test(request_params)
+
+    def test_api_delete(self):
+        """Test the quotes delete API."""
+        request_params = {
+            'data': {},
+            'view': 'quote-detail',
+            'object_id': self.verified_quote.id,
+            'method': 'delete',
+            'change_status_code': 204,
+        }
+
+        (response, change, contribution) = self.api_moderation_view_test(**request_params)
+
+        self.assertIsNotNone(change.changed_object.deleted, 'Deletion change was not created')
