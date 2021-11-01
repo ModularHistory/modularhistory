95d39fda32506da13adb4c3f799c17afe8408dd2
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-11-01 13:51:36 -0500

    cleanup

--- frontend/components/sources/PdfLink.tsx
+++ frontend/components/sources/PdfLink.tsx
@@ -1,6 +1,6 @@
 import DocumentModal from "@/components/sources/DocumentModal";
 import { styled } from "@mui/material/styles";
-import { AnchorHTMLAttributes, FC, MouseEventHandler, ReactNode, useRef, useState } from "react";
+import { AnchorHTMLAttributes, FC, MouseEventHandler, ReactNode, useState } from "react";
 
 const StyledSpan = styled("span")({
   lineHeight: "16px",
@@ -33,36 +33,12 @@ const PdfLink: FC<PdfLinkProps> = ({ anchorProps, anchorChildren }) => {
   const docHref = anchorProps.href || "";
   const [url, pageNumber] = docHref.split("#page=");
   const [modalOpen, setModalOpen] = useState(false);
-  const requestRef = useRef<Promise<any>>();
-  const [isFailure, setIsFailure] = useState(false);
-
-  // Under failure conditions, revert to rendering a normal anchor element.
-  if (!anchorProps.href || isFailure) {
-    return <a {...anchorProps}>{anchorChildren}</a>;
-  }
-
   const title = anchorProps.title;
   const { target, ...anchorPropsWithoutTarget } = anchorProps;
-
-  const fetchDocument = () => {
-    // Only send request if we have not already sent one.
-    // if (!url && !requestRef.current) {
-    //   requestRef.current = axios
-    //     .get(docHref)
-    //     .then(() => {
-    //       setDocUrl(docHref);
-    //     })
-    //     .catch((error) => {
-    //       console.error(error);
-    //       setIsFailure(true);
-    //     });
-    // }
-  };
 
   const handleClick: MouseEventHandler = (event) => {
     event.preventDefault();
     setModalOpen(true);
-    fetchDocument();
   };
 
   return (
@@ -74,7 +50,7 @@ const PdfLink: FC<PdfLinkProps> = ({ anchorProps, anchorChildren }) => {
         setOpen={setModalOpen}
         open={modalOpen}
       />
-      <StyledSpan onMouseEnter={fetchDocument} onClick={handleClick}>
+      <StyledSpan onClick={handleClick}>
         <a {...anchorPropsWithoutTarget} onClick={(event) => event.preventDefault()}>
           {anchorChildren}
         </a>
