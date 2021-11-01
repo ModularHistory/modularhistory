67a87a67b623adeac099f028e1f586423e41888f
Nick Ghuman <nishcheyjitghuman@my.unt.edu>
2021-10-31 22:19:10 -0500

    New commit

--- frontend/pages/collections/[slug].page.tsx
+++ frontend/pages/collections/[slug].page.tsx
@@ -1,16 +1,13 @@
 import ModuleUnionCard from "@/components/cards/ModuleUnionCard";
 import Layout from "@/components/Layout";
 import PageHeader from "@/components/PageHeader";
-import Pagination from "@/components/Pagination";
 import { Collection } from "@/types/modules";
 import Grid from "@mui/material/Grid";
 import axios from "axios";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { NextSeo } from "next-seo";
-import Link from "next/link";
 import { FC } from "react";
 import { Container } from "react-bootstrap";
-
 interface CollectionProps {
   collection: Collection;
 }
@@ -28,42 +25,26 @@ const CollectionDetailPage: FC<CollectionProps> = ({ collection }: CollectionPro
           "Browse historical occurrences related to your topics or entities of interest."
         }
       />
-      {[collection.propositions, collection.entities, collection.quotes, collection.sources]
-        .flat()
-        .map((module) => {
-          <Grid item key={module.slug}>
-            <Link href={module.absoluteUrl}>
-              <a>
-                <ModuleUnionCard module={module} />
-              </a>
-            </Link>
-          </Grid>;
-        })}
-      )<PageHeader>Collections</PageHeader>
-      <Pagination count={2} />
+      <PageHeader>Collections</PageHeader>
       <Container>
         <Grid container spacing={2}>
           {collection.propositions.map((proposition) => (
             <Grid item key={proposition.slug}>
-              {" "}
               <ModuleUnionCard module={proposition} />
             </Grid>
           ))}
           {collection.quotes.map((quote) => (
             <Grid item key={quote.slug}>
-              {" "}
               <ModuleUnionCard module={quote} />
             </Grid>
           ))}
           {collection.sources.map((source) => (
             <Grid item key={source.slug}>
-              {" "}
               <ModuleUnionCard module={source} />
             </Grid>
           ))}
-          {collection.entities.map((entity) => (
+          {collection.propositions.map((entity) => (
             <Grid item key={entity.slug}>
-              {" "}
               <ModuleUnionCard module={entity} />
             </Grid>
           ))}
