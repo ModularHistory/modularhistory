32e398ee8a279981b53a43b349a4c1ac9957b418
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-11-01 20:38:30 -0500

    debugging

--- frontend/components/details/__tests__/ModuleHTML.test.tsx
+++ frontend/components/details/__tests__/ModuleHTML.test.tsx
@@ -1,5 +1,5 @@
 import ModuleHTML from "@/components/details/ModuleHTML";
-import { ThemeProvider, useTheme } from "@mui/material/styles";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
 import { render, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import mockRouter from "next-router-mock";
@@ -33,7 +33,12 @@ describe("ModuleHTML", () => {
     moduleHTML: within(
       await waitFor(
         () =>
-          render(<ModuleHTML html={ReactDOMServer.renderToStaticMarkup(node)} />, options).container
+          render(
+            <ThemeProvider theme={createTheme({})}>
+              <ModuleHTML html={ReactDOMServer.renderToStaticMarkup(node)} />
+            </ThemeProvider>,
+            options
+          ).container
       )
     ),
     normal: within(render(node).container),
