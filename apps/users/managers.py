dbca894a4160b0a0d2daca1c8924d2febac4d844
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-11-04 17:52:55 -0600

    cleanup

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -30,27 +30,3 @@ class EmailAddressManager(models.Manager):
             return self.get(user=user, primary=True)
         except self.model.DoesNotExist:
             return None
-
-    def fill_cache_for_user(self, user, addresses):
-        """
-        In a multi-db setup, inserting records and re-reading them later
-        on may result in not being able to find newly inserted
-        records. Therefore, we maintain a cache for the user so that
-        we can avoid database access when we need to re-read..
-        """
-        user._emailaddress_cache = addresses
-
-    def get_for_user(self, user, email):
-        cache_key = '_emailaddress_cache'
-        addresses = getattr(user, cache_key, None)
-        if addresses is None:
-            email_address: 'EmailAddress' = self.get(user=user, email__iexact=email)
-            # To avoid additional lookups when e.g.
-            # EmailAddress.set_as_primary() starts touching self.user
-            email_address.user = user
-            return email_address
-        else:
-            for email_address in addresses:
-                if email_address.email.lower() == email.lower():
-                    return email_address
-            raise self.model.DoesNotExist()
