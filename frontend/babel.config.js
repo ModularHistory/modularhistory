da6a134ac2425175573134b7760f9cd59ed2ba67
mattfredericksen <mfred98@gmail.com>
2021-11-06 23:01:27 -0500

    comments

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,3 +1,5 @@
+// https://mui.com/guides/minimizing-bundle-size/#development-environment
+// https://github.com/umidbekk/babel-plugin-direct-import#nextjs
 module.exports = (api) => {
   const target = api.caller((caller) => caller.target);
 
