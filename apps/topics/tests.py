7074e940faf62b6b9b237248df700b57d877b3cf
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-11-09 18:19:15 -0700

    refactoring

--- apps/topics/tests.py
+++ apps/topics/tests.py
@@ -24,15 +24,15 @@ def test_children():
     """Test accessing a topic's direct children."""
     top: Topic = create_topic(name='Top')
     science = create_topic(name='Science')
-    science.add_parent(top, verified=True)
+    science.add_parent(top)
     assert isinstance(science, Topic)
     assert top in science.parents.all()
     sport = create_topic(name='Sport')
-    sport.add_parent(top, verified=True)
+    sport.add_parent(top)
     news = create_topic(name='News')
-    news.add_parent(top, verified=True)
+    news.add_parent(top)
     politics = create_topic(name='Politics')
-    politics.add_parent(news, verified=True)
+    politics.add_parent(news)
     children = top.children.all()
     assert children.exists(), f'{top} has no children: {type(children)}: {children}'
     assert list(children.order_by('name')) == [news, science, sport]
@@ -42,34 +42,34 @@ def test_descendants():
     """Test accessing a topic's descendants."""
     top = create_topic(name='Top')
     science = create_topic(name='Science')
-    science.add_parent(top, verified=True)
+    science.add_parent(top)
     maths = create_topic(name='Maths')
-    maths.add_parent(science, verified=True)
+    maths.add_parent(science)
     assert list(top.self_and_descendants.values_list('name', flat=True).order_by('name')) == [
         'Maths',
         'Science',
         'Top',
     ]
     biology = create_topic(name='Biology')
-    biology.add_parent(science, verified=True)
+    biology.add_parent(science)
     genetics = create_topic(name='Genetics')
-    genetics.add_parent(biology, verified=True)
+    genetics.add_parent(biology)
     neuroscience = create_topic(name='Neuroscience')
-    neuroscience.add_parent(biology, verified=True)
+    neuroscience.add_parent(biology)
     assert list(biology.descendants.values_list('name', flat=True).order_by('name')) == [
         'Genetics',
         'Neuroscience',
     ]
     sport = create_topic(name='Sport')
-    sport.add_parent(top, verified=True)
+    sport.add_parent(top)
     rugby = create_topic(name='Rugby')
-    rugby.add_parent(sport, verified=True)
+    rugby.add_parent(sport)
     football = create_topic(name='Football')
-    football.add_parent(sport, verified=True)
+    football.add_parent(sport)
     champions_league = create_topic(name='Champions League')
-    champions_league.add_parent(football, verified=True)
+    champions_league.add_parent(football)
     world_cup = create_topic(name='World Cup')
-    world_cup.add_parent(football, verified=True)
+    world_cup.add_parent(football)
     assert list(top.self_and_descendants.values_list('name', flat=True).order_by('name')) == [
         'Biology',
         'Champions League',
