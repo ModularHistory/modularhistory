daa42bd1707b2781ec9efd7da3e55e9ca82f46c9
mattfredericksen <mfred98@gmail.com>
2021-11-11 17:41:51 -0600

    create instant search document class factory

--- apps/search/documents/base.py
+++ apps/search/documents/base.py
@@ -1,5 +1,11 @@
+from typing import Type
+
 from django_elasticsearch_dsl import Document as ESDocument
 from django_elasticsearch_dsl import fields
+from django_elasticsearch_dsl.registries import registry
+
+from apps.search.documents.config import DEFAULT_INDEX_SETTINGS, instant_search_analyzer
+from core.models.module import Module
 
 
 class Document(ESDocument):
@@ -14,3 +20,32 @@ class Document(ESDocument):
     @classmethod
     def get_index_name(cls, index=None):
         return cls._default_index(index)
+
+
+def InstantSearchDocumentFactory(model: Type[Module], field_names: list[str], register=True):
+    instant_search_fields = {}
+    for field in field_names:
+        if not hasattr(model, field):
+            raise KeyError(f'{model} does not have attribute "{field}"')
+        instant_search_fields[field] = fields.TextField(analyzer=instant_search_analyzer)
+
+    document_class = type(
+        f'{model.__name__}InstantSearchDocument',
+        (Document,),
+        {
+            'Index': type(
+                'Index',
+                (),
+                {
+                    'name': f'{model.__name__.lower()}_instant_search',
+                    'settings': DEFAULT_INDEX_SETTINGS,
+                },
+            ),
+            'Django': type('Django', (), {'model': model}),
+            **instant_search_fields,
+        },
+    )
+
+    if register:
+        registry.register_document(document_class)
+    return document_class
--- apps/search/documents/topic.py
+++ apps/search/documents/topic.py
@@ -1,21 +1,4 @@
-from django_elasticsearch_dsl import Document as ESDocument
-from django_elasticsearch_dsl import fields
-from django_elasticsearch_dsl.registries import registry
-
-from apps.search.documents.config import DEFAULT_INDEX_SETTINGS, instant_search_analyzer
+from apps.search.documents.base import InstantSearchDocumentFactory
 from apps.topics.models.topic import Topic
 
-
-@registry.register_document
-class TopicInstantSearchDocument(ESDocument):
-    """ElasticSearch document for topic used by search-as-you-type."""
-
-    class Index:
-        settings = DEFAULT_INDEX_SETTINGS
-        name = 'topics_instant_search'
-
-    name = fields.TextField(analyzer=instant_search_analyzer)
-    aliases = fields.TextField(analyzer=instant_search_analyzer)
-
-    class Django:
-        model = Topic
+TopicInstantSearchDocument = InstantSearchDocumentFactory(Topic, ['name', 'aliases'])
