5507ac2279eca7c83c398661966c81035da1f7a7
Alashov Berkeli <yunus.alashow@gmail.com>
2021-11-11 18:39:32 -0600

    MH-271: add article wip instant search fields

--- apps/sources/api/sources/article/serializers.py
+++ apps/sources/api/sources/article/serializers.py
@@ -1,6 +1,7 @@
 from drf_writable_nested import UniqueFieldsMixin
 from rest_framework.exceptions import ValidationError
 
+from apps.search.api.instant_search import INSTANT_SEARCH_TOPICS
 from apps.sources.api.serializers import PageNumbersDrfSerializerMixin, SourceDrfSerializer
 from apps.sources.api.sources.publication.serializers import PublicationDrfSerializer
 from apps.sources.models import Article
@@ -10,6 +11,18 @@ class _ArticleDrfSerializer(
     UniqueFieldsMixin, SourceDrfSerializer, PageNumbersDrfSerializerMixin
 ):
     """Serializer for article sources."""
+
+    instant_search_fields = {
+        'publication': {
+            'module': INSTANT_SEARCH_TOPICS,  # TODO
+        },
+        'original_edition': {
+            'module': INSTANT_SEARCH_TOPICS,  # TODO
+            'filters': {
+                'model': 'sources.article',
+            },
+        },
+    }
 
     publication = PublicationDrfSerializer()
 
