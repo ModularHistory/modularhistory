ee9f166796e1bfc4f29b9d2d3a92ab34a33771a2
Alashov Berkeli <yunus.alashow@gmail.com>
2021-11-22 02:50:53 -0600

    MH-271: add source file api tests, changed sourcefile File input to base64

--- apps/sources/api/sources/file/serializers.py
+++ apps/sources/api/sources/file/serializers.py
@@ -1,9 +1,33 @@
+import filetype
+from django.utils.translation import gettext_lazy as _
+from drf_extra_fields.fields import Base64FileField
+from rest_framework import serializers
+
 from apps.sources.models import SourceFile
 from core.models.model import DrfModelSerializer
 
 
+class SourceFileBase64Field(Base64FileField):
+
+    # TODO: add additional file types if needed
+    ALLOWED_TYPES = ['pdf', 'epub', 'jpeg', 'jpg', 'png', 'gif']
+    INVALID_FILE_MESSAGE = _("Uploaded file has invalid type '{}', allowed types are: {}")
+
+    def get_file_extension(self, filename, decoded_file):
+        guessed_extension = filetype.guess_extension(decoded_file)
+        if guessed_extension not in self.ALLOWED_TYPES:
+            raise serializers.ValidationError(
+                self.INVALID_FILE_MESSAGE.format(
+                    guessed_extension, ', '.join(self.ALLOWED_TYPES)
+                )
+            )
+        return guessed_extension
+
+
 class SourceFileDrfSerializer(DrfModelSerializer):
     """Serializer for sources files."""
+
+    file = SourceFileBase64Field()
 
     class Meta:
         model = SourceFile
