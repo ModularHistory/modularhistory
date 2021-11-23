9a62c057ef4f78d43a4113d9a03388cbdaa2f5b6
Sanjay Prajapati <sunzae@gmail.com>
2021-11-23 12:09:35 -0600

    refactoring

--- frontend/pages/users/[handle]/contributions.page.tsx
+++ frontend/pages/users/[handle]/contributions.page.tsx
@@ -1,18 +1,20 @@
 import axiosWithoutAuth from "@/axiosWithoutAuth";
+import ModuleUnionCard from "@/components/cards/ModuleUnionCard";
 import Layout from "@/components/Layout";
-import { Container } from "@mui/material";
+import { Card, CardContent, CardHeader, Container, Skeleton } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { GetServerSideProps } from "next";
 import { User } from "next-auth";
 import { getSession, useSession } from "next-auth/client";
+import Link from "next/link";
 import React, { FC } from "react";
 import Image from "react-bootstrap/Image";
 
 interface UserContributionsPageProps {
   user?: User;
-  usercontributions: any;
+  usercontributions: any[];
 }
 
 const UserContributionsPage: FC<UserContributionsPageProps> = ({
@@ -52,6 +54,33 @@ const UserContributionsPage: FC<UserContributionsPageProps> = ({
                   {" "}
                   My Contributions{" "}
                 </Typography>
+                <>
+                  {usercontributions.length ? (
+                    usercontributions.map((module, index) => (
+                      <Grid item key={index}>
+                        <Link href={module.absoluteUrl}>
+                          <a>
+                            <ModuleUnionCard module={module} />
+                          </a>
+                        </Link>
+                      </Grid>
+                    ))
+                  ) : _loading ? (
+                    <Grid item>
+                      <Card sx={{ minWidth: "15rem" }}>
+                        <CardHeader title={"Fetching content"} hidden />
+                        <CardContent>
+                          <Skeleton sx={{ minHeight: 200, width: "100%" }} />
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ) : (
+                    <p>
+                      Sorry! There are no content contributions made. If you`&apos;`d like to
+                      contribute, please visit (link).
+                    </p>
+                  )}
+                </>
               </Paper>
             </Grid>
           </Grid>
@@ -67,7 +96,7 @@ export default UserContributionsPage;
 // https://nextjs.org/docs/basic-features/data-fetching#getserversideprops-server-side-rendering
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
-  let usercontributions = null;
+  let usercontributions: UserContributionsPageProps[] = [];
   if (!session?.user) {
     return {
       redirect: {
