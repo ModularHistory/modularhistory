4b4a7981fbd6274e8b3e5eaf7a789c30fdf04f85
sunzae99 <70658409+sunzae99@users.noreply.github.com>
2021-12-02 17:54:47 -0600

    MH-283: Server-side render the TodayInHistory and FeaturedContent components (#885)
    * MH-283: Initial Commit
    
    * CoChanged useEffect on TIH and Featured Content to use SSR
    
    * Updated TIH Jest Tests
    
    * refactoring
    
    * fix broken test
    
    Co-authored-by: Jacob Fredericksen <jacob.t.fredericksen@gmail.com>

--- frontend/components/TodayInHistory.tsx
+++ frontend/components/TodayInHistory.tsx
@@ -1,59 +1,29 @@
-import axiosWithoutAuth from "@/axiosWithoutAuth";
-import { ModuleUnion, Topic } from "@/types/modules";
-import { Card, CardContent, CardHeader, Grid, Skeleton } from "@mui/material";
-import axios from "axios";
+import { SerpModule } from "@/types/modules";
+import { Grid } from "@mui/material";
 import Link from "next/link";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 import ModuleUnionCard from "./cards/ModuleUnionCard";
 
-const TodayInHistory: FC = () => {
-  const [items, setItems] = useState<Exclude<ModuleUnion, Topic>[]>([]);
-  const [loading, setLoading] = useState(true);
+export interface TodayInHistoryProps {
+  modules: SerpModule[];
+}
 
-  useEffect(() => {
-    const cancelTokenSource = axios.CancelToken.source();
-    axiosWithoutAuth
-      .get("/api/home/today_in_history/", { cancelToken: cancelTokenSource.token })
-      .then((response) => {
-        setItems(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        if (axios.isCancel(error)) return;
-        console.error(error);
-        setLoading(false);
-      });
-    return () => {
-      cancelTokenSource.cancel("component unmounted");
-    };
-  }, [loading]);
-
+const TodayInHistory: FC<TodayInHistoryProps> = ({ modules }: TodayInHistoryProps) => {
   return (
     <Grid container spacing={1} justifyContent="center" alignItems="center">
-      <>
-        {items.length ? (
-          items.map((module, index) => (
-            <Grid item key={index}>
-              <Link href={module.absoluteUrl}>
-                <a>
-                  <ModuleUnionCard module={module} />
-                </a>
-              </Link>
-            </Grid>
-          ))
-        ) : loading ? (
-          <Grid item>
-            <Card sx={{ minWidth: "15rem" }}>
-              <CardHeader title={"Fetching content"} hidden />
-              <CardContent>
-                <Skeleton sx={{ minHeight: 200, width: "100%" }} />
-              </CardContent>
-            </Card>
+      {modules.length ? (
+        modules.map((module, index) => (
+          <Grid item key={index}>
+            <Link href={module.absoluteUrl}>
+              <a>
+                <ModuleUnionCard module={module} />
+              </a>
+            </Link>
           </Grid>
-        ) : (
-          <p>There are no modules associated with this date.</p>
-        )}
-      </>
+        ))
+      ) : (
+        <p>There are no modules associated with this date.</p>
+      )}
     </Grid>
   );
 };
