7af23f78b15bec9b13d118ee72f0063058d68b84
Jacob Fredericksen <jacob.t.fredericksen@gmail.com>
2021-12-02 15:53:46 -0600

    cleanup

--- config/scripts/install-deps.sh
+++ config/scripts/install-deps.sh
@@ -57,7 +57,6 @@ echo "Activating virtual environment ..."
 set -a
 # shellcheck disable=SC1091
 source .venv/bin/activate
-.venv/bin/activate
 unset a
 IN_VENV=$(python -c 'import sys; print ("1" if sys.prefix != sys.base_prefix or hasattr(sys, "real_prefix") else "0")')
 if [[ "$IN_VENV" = 0 ]]; then
